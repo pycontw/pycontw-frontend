ed77f97e7908c0a014022f531de9ea5fe900d992
Flynn Hou <flynnhou7@gmail.com>
2020-12-14 00:50:08 +0800

    feat(ia): outline navigation bar
    Note that the dropdown is not finished yet.

---
+++ components/core/header/nav-bar/NavBarItemDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="relative flex flex-col items-start cursor-default">
+        <div @mouseenter="isHovering = true" @mouseleave="isHovering = false">
+            {{ title }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CoreHeaderNavBarItemDropdown',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isHovering: false,
+        }
+    },
+    computed: {
+        dropdownMenuClasses() {
+            return {
+                hidden: !this.isHovering,
+            }
+        },
+    },
+}
+</script>
+
+<style scoped></style>
