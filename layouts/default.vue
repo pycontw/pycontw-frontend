f7df77e67516776be78a960eebba080738587498
Flynn Hou <flynnhou7@gmail.com>
2020-12-14 00:56:19 +0800

    feat(layout): use grid to layout the header, body, footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
-    <div>
+    <div class="default-layout">
+        <core-header />
         <Nuxt />
+        <core-footer />
     </div>
 </template>
+
+<script>
+import CoreFooter from '~/components/core/footer'
+import CoreHeader from '~/components/core/header'
+
+export default {
+    components: {
+        CoreHeader,
+        CoreFooter,
+    },
+}
+</script>
 
 <style>
 html {
@@ -24,32 +38,10 @@ html {
     margin: 0;
 }
 
-.button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-}
-
-.button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-}
-
-.button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-}
-
-.button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
+.default-layout {
+    display: grid;
+    height: 100vh;
+    grid-template-rows: 60px auto 280px;
+    grid-template-columns: auto;
 }
 </style>
