e9ced4edf760c661588f9e7335e1628d1ffb1e19
Flynn Hou <flynnhou7@gmail.com>
2020-12-14 00:51:11 +0800

    feat(header): mock a home icon for now

---
+++ components/core/header/HomeIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLink to="/" class="w-10">
+        <svg viewBox="0 0 36 36">
+            <rect width="36" height="36" x="0" fill="#ab320e"></rect>
+        </svg>
+    </NuxtLink>
+</template>
+
+<script>
+export default {
+    name: 'HomeIcon',
+}
+</script>
+
+<style scoped></style>
