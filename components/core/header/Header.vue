3638d890a12890c7c8cf246b67a0dfda1dfc655a
Flynn Hou <flynnhou7@gmail.com>
2021-01-17 21:46:58 +0800

    feat(i18n): add locale-switch component to change langs

--- components/core/header/Header.vue
+++ components/core/header/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="w-full h-16 px-4 flex justify-between items-center">
         <home-icon />
+        <locale-switch />
         <nav-bar />
         <text-button
             href="https://tw.pycon.org/2020/en-us/dashboard/"
@@ -14,12 +15,14 @@
 <script>
 import HomeIcon from './HomeIcon'
 import NavBar from './nav-bar'
+import LocaleSwitch from './locale-switch'
 import TextButton from '~/components/core/buttons/TextButton'
 
 export default {
     name: 'CoreHeader',
     components: {
         HomeIcon,
+        LocaleSwitch,
         NavBar,
         TextButton,
     },
