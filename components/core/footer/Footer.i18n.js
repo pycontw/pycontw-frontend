12ff5e26eacfb06ef0b207e399b2ea0e2b63b6e0
Flynn Hou <flynnhou7@gmail.com>
2021-01-17 21:57:05 +0800

    fix(footer): translate items and navigate pages with proper lang

---
+++ components/core/footer/Footer.i18n.js
@@ -0,0 +1,16 @@
+import { genI18nMessages } from '~/utils/i18n.utils'
+
+export default genI18nMessages({
+    en: {
+        codeOfConduct: 'Code of Conduct',
+        staff: 'Staff',
+        community: 'Community',
+        privacyPolicy: 'Privacy Policy',
+    },
+    zh: {
+        codeOfConduct: '行為準則',
+        staff: '工作人員',
+        community: '社群',
+        privacyPolicy: '個人資料保護聲明',
+    },
+})
