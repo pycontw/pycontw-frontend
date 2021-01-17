c03d006963dbe9824530a8a7ef475a1d72a6ea9f
Flynn Hou <flynnhou7@gmail.com>
2021-01-17 21:55:44 +0800

    fix(home-icon): use LocaleLink to redirect home under a lang

--- components/core/header/HomeIcon.vue
+++ components/core/header/HomeIcon.vue
@@ -1,14 +1,19 @@
 <template>
-    <NuxtLink to="/" class="w-10">
+    <LocaleLink to="/" class="w-10">
         <svg viewBox="0 0 36 36">
             <rect width="36" height="36" x="0" fill="#ab320e"></rect>
         </svg>
-    </NuxtLink>
+    </LocaleLink>
 </template>
 
 <script>
+import LocaleLink from '@/components/core/links/LocaleLink'
+
 export default {
     name: 'HomeIcon',
+    components: {
+        LocaleLink,
+    },
 }
 </script>
 
