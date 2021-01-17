4877c3fd222f4e3fde1f859e0bd2ff99d7041077
Flynn Hou <flynnhou7@gmail.com>
2021-01-18 01:42:10 +0800

    feat(menu): add core-menu and core-menu-item

---
+++ components/core/menu/MenuItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="py-1" @click="onClick">
+        <div v-if="readonly">
+            <slot></slot>
+        </div>
+        <locale-link
+            v-else
+            :to="href"
+            class="block px-4 py-2 text-sm text-gray-700 rounded hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+        >
+            <slot></slot>
+        </locale-link>
+    </div>
+</template>
+
+<script>
+import { LocaleLink } from '~/components/core/links'
+
+export default {
+    name: 'MenuItem',
+    components: {
+        LocaleLink,
+    },
+    props: {
+        href: {
+            type: String,
+            default: '#',
+        },
+    },
+    computed: {
+        readonly() {
+            return !this.href
+        },
+    },
+    methods: {
+        onClick(event) {
+            this.$emit('click', event)
+        },
+    },
+}
+</script>
+
+<style scoped></style>
