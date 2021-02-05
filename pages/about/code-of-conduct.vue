381fecbff3f9c758f07ddce7371d039999fc9d78
Matt.Wang <mattwang44@gmail.com>
2021-02-06 01:12:27 +0800

    feat(about): add content on CoC page

--- pages/about/code-of-conduct.vue
+++ pages/about/code-of-conduct.vue
@@ -1,9 +1,29 @@
 <template>
-    <p>CODE OF CONDUCT</p>
+    <div class="px-48 py-24">
+        <h1 class="text-3xl my-6 text-center">
+            2021 {{ $t('codeOfConduct') }}
+        </h1>
+        <p class="my-6" v-html="$t('paragraph.intro')"></p>
+        <h2 class="text-2xl my-6 text-center">{{ $t('codeOfConduct') }}</h2>
+        <ul>
+            <li
+                v-for="(content, idx) in $t('paragraph.contents')"
+                :key="idx"
+                class="my-4"
+            >
+                <b>{{ content.title }}</b>
+                {{ content.description }}
+            </li>
+        </ul>
+        <p class="my-6" v-html="$t('paragraph.summary')"></p>
+    </div>
 </template>
 
 <script>
+import i18n from './code-of-conduct.i18n'
+
 export default {
+    i18n,
     name: 'PageCodeOfConduct',
 }
 </script>
