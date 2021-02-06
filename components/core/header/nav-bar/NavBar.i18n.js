8e766275979d6886df707e4dc3b1d828159a01b1
Matt.Wang <mattwang44@gmail.com>
2021-02-06 15:05:49 +0800

    feat(nav-bar): add speaking item

--- components/core/header/nav-bar/NavBar.i18n.js
+++ components/core/header/nav-bar/NavBar.i18n.js
@@ -3,6 +3,11 @@ import { genI18nMessages } from '~/utils/i18n.utils'
 export default genI18nMessages({
     'en-us': {
         about: 'About',
+        speaking: 'Speaking',
+        cfp: 'Call for Proposals',
+        talk: 'Propose a Talk',
+        tutorial: 'Propose a Tutorial',
+        recording: 'Recording Release',
         conference: 'Conference',
         schedule: 'Schedule',
         keynotes: 'Keynotes',
@@ -23,6 +28,11 @@ export default genI18nMessages({
     },
     'zh-hant': {
         about: '關於',
+        speaking: '投稿',
+        cfp: '投稿募集',
+        talk: '如何投稿演講',
+        tutorial: '如何投稿專業課程',
+        recording: '錄影釋出',
         conference: '議程',
         schedule: '時間表',
         keynotes: '基調演講',
