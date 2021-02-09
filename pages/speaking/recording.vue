7546867e51bb9ed3c7dfd28f94dae98a634b0d84
Matt.Wang <mattwang44@gmail.com>
2021-02-09 12:46:42 +0800

    feat(speaking): add recording content

--- pages/speaking/recording.vue
+++ pages/speaking/recording.vue
@@ -1,10 +1,32 @@
 <template>
-    <p>Recording</p>
+    <div class="px-64 py-24">
+        <h1 class="text-3xl my-8 text-center">{{ $t('title') }}</h1>
+        <i18n
+            v-for="(paragraph, i) in $t('paragraphs')"
+            :key="i"
+            :path="`paragraphs.${i}`"
+            tag="p"
+            class="my-8"
+        >
+            <template #pyvideo>
+                <ext-link href="https://pyvideo.org" :highlight="true"
+                    >pyvideo.org</ext-link
+                >
+            </template>
+        </i18n>
+    </div>
 </template>
 
 <script>
+import i18n from './recording.i18n'
+import ExtLink from '~/components/core/links/ExtLink'
+
 export default {
+    i18n,
     name: 'PageSpeakingRecording',
+    components: {
+        ExtLink,
+    },
 }
 </script>
 
