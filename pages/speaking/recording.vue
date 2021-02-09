b50062f4c4668e678daf6cf47ec2cd88ca52d44c
Matt.Wang <mattwang44@gmail.com>
2021-02-09 12:46:42 +0800

    feat(speaking): add recording content

--- pages/speaking/recording.vue
+++ pages/speaking/recording.vue
@@ -1,9 +1,20 @@
 <template>
-    <p>Recording</p>
+    <div class="px-48 py-24">
+        <h1 class="text-3xl my-6 text-center">{{ $t('title') }}</h1>
+        <p
+            v-for="(content, idx) in $t('descriptions')"
+            :key="idx"
+            class="my-6"
+            v-html="content"
+        ></p>
+    </div>
 </template>
 
 <script>
+import i18n from './recording.i18n'
+
 export default {
+    i18n,
     name: 'PageSpeakingRecording',
 }
 </script>
