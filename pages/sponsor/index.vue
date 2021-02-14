98474c126d29f86214b2318549955c15cb941ef9
Matt.Wang <mattwang44@gmail.com>
2021-02-14 17:16:26 +0800

    feat(sponsor): add sponsor related pages

---
+++ pages/sponsor/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Sponsor</p>
+</template>
+
+<script>
+export default {
+    name: 'PageSponsor',
+}
+</script>
+
+<style scoped></style>
---
+++ pages/sponsor/prospectus.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Sponsor Prospectus</p>
+</template>
+
+<script>
+export default {
+    name: 'PageSponsorProspectus',
+}
+</script>
+
+<style scoped></style>
