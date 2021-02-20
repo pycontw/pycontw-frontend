e6bc69ac79edd7bcc03883f5440ac4cc431d94f6
Flynn Hou <flynnhou7@gmail.com>
2021-02-20 20:09:31 +0800

    feat(vuex): use store for reactive jobsData

--- store/index.js
+++ store/index.js
@@ -2,15 +2,21 @@ import { set } from '~/utils/store.utils'
 
 export const state = () => ({
     sponsorsData: [],
+    jobsData: [],
 })
 
 export const mutations = {
     setSponsorsData: set('sponsorsData'),
+    setJobsData: set('jobsData'),
 }
 
 export const actions = {
     async $getSponsorsData({ commit }) {
-        const { data: sponsorsData } = await this.$http.$get('/api/sponsors')
-        commit('setSponsorsData', sponsorsData)
+        const { data } = await this.$http.$get('/api/sponsors')
+        commit('setSponsorsData', data)
+    },
+    async $getJobsData({ commit }) {
+        const { data } = await this.$http.$get('/api/jobs')
+        commit('setJobsData', data)
     },
 }
