e02bd3a6c34b905edcc802dd670ecb4f1af763cd
Flynn Hou <flynnhou7@gmail.com>
2021-02-21 20:31:13 +0800

    chore: comment out sponsor list for now

--- pages/index.vue
+++ pages/index.vue
@@ -5,47 +5,47 @@
                 <h1 class="text-6xl">{{ $t('pyconComingSoon') }}</h1>
             </div>
         </div>
-        <h3>sponsor list</h3>
-        <div
-            v-for="(leveledSponsors, i) in sponsorsData"
-            :key="`index_sponsor_level_${i}`"
-        >
-            <h2 class="text-2xl">{{ leveledSponsors.level_name }}</h2>
-            <div
-                v-for="(sponsor, j) in leveledSponsors.sponsors"
-                :key="`index_sponsor_level_${i}_sponsor_${j}`"
-                class="border-black"
-            >
-                <div>
-                    Name:
-                    <a :href="sponsor.website_url">{{ sponsor.name }}</a>
-                </div>
-                <div>Intro: {{ sponsor.intro }}</div>
-                <img
-                    :src="`https://5f43c00e6788.ngrok.io${sponsor.logo_url}`"
-                    :width="40"
-                    :height="30"
-                    alt=""
-                />
-            </div>
-        </div>
+        <!--        <h3>sponsor list</h3>-->
+        <!--        <div-->
+        <!--            v-for="(leveledSponsors, i) in sponsorsData"-->
+        <!--            :key="`index_sponsor_level_${i}`"-->
+        <!--        >-->
+        <!--            <h2 class="text-2xl">{{ leveledSponsors.level_name }}</h2>-->
+        <!--            <div-->
+        <!--                v-for="(sponsor, j) in leveledSponsors.sponsors"-->
+        <!--                :key="`index_sponsor_level_${i}_sponsor_${j}`"-->
+        <!--                class="border-black"-->
+        <!--            >-->
+        <!--                <div>-->
+        <!--                    Name:-->
+        <!--                    <a :href="sponsor.website_url">{{ sponsor.name }}</a>-->
+        <!--                </div>-->
+        <!--                <div>Intro: {{ sponsor.intro }}</div>-->
+        <!--                <img-->
+        <!--                    :src="`https://5f43c00e6788.ngrok.io${sponsor.logo_url}`"-->
+        <!--                    :width="40"-->
+        <!--                    :height="30"-->
+        <!--                    alt=""-->
+        <!--                />-->
+        <!--            </div>-->
+        <!--        </div>-->
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+// import { mapState } from 'vuex'
 import i18n from '@/i18n/index.i18n'
 
 export default {
     i18n,
     name: 'PageIndex',
-    fetchOnServer: false,
-    computed: {
-        ...mapState(['sponsorsData']),
-    },
-    created() {
-        this.$store.dispatch('$getSponsorsData')
-    },
+    // fetchOnServer: false,
+    // computed: {
+    //     ...mapState(['sponsorsData']),
+    // },
+    // created() {
+    //     this.$store.dispatch('$getSponsorsData')
+    // },
 }
 </script>
 
