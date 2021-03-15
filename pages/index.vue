193a4ab59195e429f585432cb18e57df60b6caa8
Ethan Lin <ethan.lin@ibm.com>
2021-03-16 03:27:05 +0800

    Add AnnounceBar

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container">
             <div>
-                <h1 class="text-6xl">{{ $t('pyconComingSoon') }}</h1>
+                <h1 class="text-6xl">{{ $t('pyconWelcome') }}</h1>
             </div>
         </div>
         <!--        <h3>sponsor list</h3>-->
