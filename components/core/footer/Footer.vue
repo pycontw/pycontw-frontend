adb9de7e8b06b1a2969b2bd387a3eac530932514
本本 <thicyo@gmail.com>
2021-03-17 15:24:31 +0800

    add new custom color

--- components/core/footer/Footer.vue
+++ components/core/footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col px-4 py-8 bg-orange-300">
+    <div class="flex flex-col px-4 py-8 bg-dark-primary text-golden">
         <div class="flex justify-center">
             <div class="flex flex-col mr-20">
                 <locale-link to="/about/code-of-conduct">
