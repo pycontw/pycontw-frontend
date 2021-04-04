a5a22af1379cf5516edd6dc0cf7c553967f5702c
mattwang44 <mattwang44@gmail.com>
2021-04-04 17:07:18 +0000

    Deploying to gh-pages from  @ fda2f87f181a546bbc35ca24ec2ce203715756b9 🚀

---
+++ _nuxt/static/1617556034/conference/keynotes/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1617556034",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fconference\u002Fkeynotes",config:{}};
