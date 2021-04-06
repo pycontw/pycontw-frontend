dd5dc93b7c826436ab1fcae849c88fa3fdbce649
Matt.Wang <mattwang44@gmail.com>
2021-04-06 22:52:38 +0800

    fix(favicon): correct favicon link

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,9 @@ export default {
             },
             { hid: 'description', name: 'description', content: '' },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/2021/favicon.ico' },
+        ],
     },
 
     // Global CSS (https://go.nuxtjs.dev/config-css)
