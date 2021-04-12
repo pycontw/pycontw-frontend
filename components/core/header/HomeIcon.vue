bbe80dcfcfe6b125aa9f2de5897106e540d82c80
Matt.Wang <mattwang44@gmail.com>
2021-04-12 21:08:21 +0800

    fix(static): correct the url of images in static folder

--- components/core/header/HomeIcon.vue
+++ components/core/header/HomeIcon.vue
@@ -4,7 +4,7 @@
         class="md:px-12 py-2 flex justify-center items-center bg-golden-primary"
         customized
     >
-        <img src="/snake-icon.svg" class="w-8" />
+        <img src="~/static/snake-icon.svg" class="w-8" />
         <span class="font-bold"> PyConTW </span>
     </locale-link>
 </template>
