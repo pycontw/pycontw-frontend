7b28cd3233a43766914be5f98ef6a6df34bea255
flynnhou <flisshou@gmail.com>
2021-04-13 13:26:54 +0000

    Deploying to gh-pages from  @ 637c12bb16e21fee0adda01d59ad026af32e150d 🚀

--- _nuxt/static/1618245397/covid-19/guidelines/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1618245397",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fcovid-19\u002Fguidelines",config:{}};
