aad3de7694928299e67fe37f8d9c31670cbcd2c8
mattwang44 <mattwang44@gmail.com>
2021-04-22 13:06:53 +0000

    Deploying to gh-pages from  @ 5d350b3d4325cf00fe4c85dbfd1ed9ea2a140de3 🚀

---
+++ _nuxt/static/1619096806/zh-hant/about/privacy-policy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1619096806",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fzh-hant\u002Fabout\u002Fprivacy-policy",config:{}};
