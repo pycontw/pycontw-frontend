630a5d016e55226ca928e5c28dc99bcbd34efad2
Flynn Hou <flynnhou7@gmail.com>
2021-05-02 23:03:43 +0800

    fix(landing-page): revamp the bg-img, texts, slogan

---
+++ components/landing-focus/LandingFocusInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="landingFocusInfo flex flex-col items-center font-serif">
+        <div class="landingFocusInfo__temporal">8/20</div>
+        <div class="landingFocusInfo__till"></div>
+        <div class="landingFocusInfo__temporal">8/22</div>
+        <div class="landingFocusInfo__locale mt-4">NTU</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LandingFocusInfo',
+}
+</script>
+
+<style scoped>
+.landingFocusInfo__till {
+    @apply h-5 my-2;
+    border-left: 2px solid #c2a53a;
+}
+
+.landingFocusInfo__temporal {
+    @apply inline-flex w-12 h-12 p-2 justify-center items-center rounded-full border;
+    border: 4px solid #c2a53a;
+}
+</style>
---
+++ components/landing-focus/LandingFocusSlogan.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center">
+        <pre
+            class="text-xs mr-4"
+        ><code>with</code> Pythonistas() <code>as</code> us:
+          Python.TW.reunion()</pre>
+        <div class="w-10 md:w-16 lg:w-20">
+            <img
+                src="~/static/page-home-slogan.png"
+                alt=""
+                class="object-contain"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LandingFocusSlogan',
+}
+</script>
+
+<style scoped>
+code {
+    color: #7568f6;
+}
+</style>
