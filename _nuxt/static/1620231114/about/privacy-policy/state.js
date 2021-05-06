d56ad5ea7ba6fb14938a4ad91453362ed5db4089
mattwang44 <mattwang44@gmail.com>
2021-05-06 17:11:07 +0000

    Deploying to gh-pages from  @ e4a723234109118f153266072f4d3596d5e6018e 🚀

--- _nuxt/static/1620231114/about/privacy-policy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1620231114",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fabout\u002Fprivacy-policy",config:{}};
