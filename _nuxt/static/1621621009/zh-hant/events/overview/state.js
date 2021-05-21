d12c10c9713ff7c6046ac86c404c597614a1e528
flynnhou <flisshou@gmail.com>
2021-05-21 18:16:56 +0000

    Deploying to gh-pages from  @ c5d3a445a2b279c6e9bbe24983df14b0289ec3bc 🚀

---
+++ _nuxt/static/1621621009/zh-hant/events/overview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1621621009",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fzh-hant\u002Fevents\u002Foverview",config:{}};
