e434ed7b9f0d4a8e759885b7c7ffffd6e2b2cddd
Flynn Hou <flynnhou7@gmail.com>
2021-05-24 02:31:27 +0800

    feat(jobs): add jobs-card, jobs-card-collection, jobs-panel

---
+++ components/jobs/JobsCardCollection.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="jobsCardCollection">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JobsCardCollection',
+}
+</script>
+
+<style scoped>
+.jobsCardCollection {
+    @apply sticky z-50 flex w-full px-0 py-4 flex-wrap justify-center items-center;
+    top: 48px;
+    background-color: #212121;
+}
+</style>
---
+++ components/jobs/JobsPanel.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="jobsPanel">
+        <div
+            v-for="(job, i) in jobs"
+            :key="`jobs_panel_job_${i}`"
+            class="jobsPanelJob"
+        >
+            <div class="jobsPanelJob__title">
+                {{ job[`job_name_${locale}`] || '-' }}
+            </div>
+            <div class="jobsPanelJob__description">
+                {{ job[`job_description_${locale}`] || '-' }}
+            </div>
+            <div class="jobsPanelJob__requirements">
+                {{ job[`job_requirements_${locale}`] || '-' }}
+            </div>
+            <core-text-button :href="job.job_url" class="mt-4 mb-8">
+                {{ ctaLabel }}
+            </core-text-button>
+        </div>
+        <div v-if="!jobs.length" class="jobsPanelEmpty">
+            <div class="jobsPanelJob__description">{{ emptyMessage }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CoreTextButton from '@/components/core/buttons/TextButton'
+
+export default {
+    name: 'JobsPanel',
+    components: {
+        CoreTextButton,
+    },
+    props: {
+        jobs: { type: Array, default: () => [] },
+        ctaLabel: { type: String, default: '' },
+        emptyMessage: { type: String, default: '' },
+    },
+    computed: {
+        locale() {
+            return { 'en-us': 'en_us', 'zh-hant': 'zh_hant' }[this.$i18n.locale]
+        },
+    },
+}
+</script>
+
+<style scoped>
+.jobsPanel {
+    @apply relative p-8 mb-12 rounded-2xl;
+    background-color: #1d1a37;
+}
+
+.jobsPanelJob__title {
+    @apply mb-4 font-bold text-2xl;
+    color: #c2a53a;
+}
+
+.jobsPanelJob__description,
+.jobsPanelJob__requirements {
+    @apply whitespace-pre-line;
+    color: #ffffff;
+}
+</style>
