3299dae68f16b63a80ab3078351c0f1622c6f0aa
flynnhou <flisshou@gmail.com>
2021-05-30 08:37:25 +0000

    Deploying to gh-pages from  @ cf38a873834ca70a7c4916218b699044f4a6fe89 🚀

---
+++ _nuxt/static/1622363836/zh-hant/speaking/recording/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1622363836",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fzh-hant\u002Fspeaking\u002Frecording",config:{gtm:{id:""}}};
