f655a49025fe7d25999a06975afbc9c3385ebcbb
張櫻瀞 <alice6373@gmail.com>
2021-05-30 18:52:31 +0800

    feat(bulletin/collection): add bullient card collection component

---
+++ components/bulletins/BulletinCardCollection.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex flex-col w-full">
+        <div
+            class="bulletinCardCollection__cardContainer justify-center md:justify-start"
+        >
+            <!-- <slot></slot> -->
+            <bulletin-card
+                v-for="(bulletin, i) in $t('bulletins')"
+                :key="`index_bulletin_${i}`"
+                :title="bulletin.title"
+                :description="bulletin.description"
+                :linkStr="bulletin.linkStr"
+                :linkUrl="bulletin.linkUrl"
+            >
+            </bulletin-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import i18n from './BulletinCard.i18n'
+export default {
+    i18n,
+    name: 'BulletinCardCollection',
+}
+</script>
+
+<style lang="postcss" scoped>
+.bulletinCardCollection__cardContainer {
+    @apply flex flex-wrap;
+}
+
+.bulletinCardCollection__cardContainer > .bulletinCard {
+    margin: 0 0 60px 0;
+    padding: 5px;
+    @media (min-width: 415px) {
+        margin: 5px 15px 65px 5px;
+    }
+}
+
+.bulletinCardCollection__cardContainer > .bulletinCard:hover {
+    @apply cursor-pointer box-content;
+    margin: 0 10px 60px 0;
+}
+</style>
