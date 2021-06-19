6fe9c40431ac50af16018a81b6b3c9e76f3c9bd4
Ethan Lin <ethan.lin@ibm.com>
2021-06-19 15:43:06 +0800

    fix: head function problem with gtm module

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,57 @@ export default {
         CoreFooter,
         AnnounceBar,
     },
+    head() {
+        return {
+            titleTemplate: (titleChunk) => {
+                const title = 'PyCon Taiwan 2021'
+                return titleChunk && titleChunk !== title
+                    ? `${titleChunk} | ${title}`
+                    : title
+            },
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                { hid: 'description', name: 'description', content: '' },
+                {
+                    hid: 'og:title',
+                    property: 'og:title',
+                    content: 'PyCon Taiwan 2021',
+                },
+                {
+                    hid: 'og:description',
+                    property: 'og:description',
+                    content: 'PyCon Taiwan 2021',
+                },
+                {
+                    hid: 'og:image',
+                    property: 'og:image',
+                    content: '/og-image.jpg',
+                },
+                { hid: 'og:type', property: 'og:type', content: 'website' },
+            ],
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    href: '/2021/favicon.ico',
+                },
+                {
+                    rel: 'stylesheet',
+                    href:
+                        'https://fonts.googleapis.com/css?family=Noto%20Serif%20TC',
+                },
+
+                {
+                    rel: 'canonical',
+                    href: 'https://tw.pycon.org' + this.$route.path,
+                },
+            ],
+        }
+    },
 }
 </script>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,57 +11,8 @@ export default {
         base: process.env.ROUTER_BASE || DEFAULT_ROUTER_BASE,
     },
     // Global page headers (https://go.nuxtjs.dev/config-head)
-    head() {
-        return {
-            titleTemplate: (titleChunk) => {
-                const title = 'PyCon Taiwan 2021'
-                return titleChunk && titleChunk !== title
-                    ? `${titleChunk} | ${title}`
-                    : title
-            },
-            meta: [
-                { charset: 'utf-8' },
-                {
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1',
-                },
-                { hid: 'description', name: 'description', content: '' },
-                {
-                    hid: 'og:title',
-                    property: 'og:title',
-                    content: 'PyCon Taiwan 2021',
-                },
-                {
-                    hid: 'og:description',
-                    property: 'og:description',
-                    content: 'PyCon Taiwan 2021',
-                },
-                {
-                    hid: 'og:image',
-                    property: 'og:image',
-                    content: '/og-image.jpg',
-                },
-                { hid: 'og:type', property: 'og:type', content: 'website' },
-            ],
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/2021/favicon.ico',
-                },
-                {
-                    rel: 'stylesheet',
-                    href:
-                        'https://fonts.googleapis.com/css?family=Noto%20Serif%20TC',
-                },
-
-                {
-                    rel: 'canonical',
-                    href: 'https://tw.pycon.org' + this.$route.path,
-                },
-            ],
-        }
-    },
+    // Move to layout/default.vue due to gtm-module not support head function (https://github.com/nuxt-community/gtm-module/issues/56)
+    head: {},
 
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [],
