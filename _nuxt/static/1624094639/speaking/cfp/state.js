accf33755142d7fba976b4c3eba83ab193309b7a
mattwang44 <mattwang44@gmail.com>
2021-06-19 09:24:10 +0000

    Deploying to gh-pages from  @ 92692c6410ba2e682a9acfda88aca1bfdf5c1336 🚀

---
+++ _nuxt/static/1624094639/speaking/cfp/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fpycontw-2021\u002F_nuxt\u002Fstatic\u002F1624094639",layout:"default",error:null,state:{sponsorsData:[],jobsData:[],i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fspeaking\u002Fcfp",config:{gtm:{id:""}}};
