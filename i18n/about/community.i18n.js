b4e379ba4d2ce7dc0f5bf7716431c9b95e5aafe5
Matt.Wang <mattwang44@gmail.com>
2021-06-20 10:12:04 +0800

    chore: rephrase en community description

--- i18n/about/community.i18n.js
+++ i18n/about/community.i18n.js
@@ -8,64 +8,106 @@ export default genI18nMessages({
                 tag: 'taipei',
                 title: 'Taipei - Taipei.py',
                 desc: [
-                    'After COSCUP 2012, Keith, Tim and some enthusiasts launched Taipei.py. The number of members rapidly grew up to 50 after the first few meetups. The events are hosted monthly at CLBC. The topics are mainly related to web programming and web application. Recently there are more sharing of big data topics. Since most of the organizers are in web-related industry, Andy also hosts other activities, such as Python Web Meetup and Django study group, besides the monthly meetups.',
+                    'Keith, Tim, and some enthusiasts launched Taipei.py in 2012 and ' +
+                        'started hosting monthly events at CLBC. ' +
+                        'The topics are mainly focused on web technology and data science. ' +
+                        'Besides the monthly meetups, Andy also hosted Python Web Meetup and ' +
+                        'Django study group for web geeks!',
                 ],
             },
             {
                 tag: 'pyhug',
-                title: 'Hsinchu - Python Hsinchu User Group (PyHUG)',
+                title: 'Hsinchu - PyHUG',
                 desc: [
-                    'The first Python community in Taiwan, founded by yyc and Albert Huang in 2011. Speakers are invited to share regularly every month. The participating members are mainly college students, and the gathering emphasizes the application of Python in science.',
-                    'In recent years, gatherings in cafes every Wednesday has gradually become a form of "eating, drinking, talking about Python and all sorts of things". Dainese is currently in charge, and new blood from the place is needed to join.',
+                    'Taiwan’s first Python community was established in 2011 by yyc and ' +
+                        'Albert Huang. Speakers are invited to share every month. ' +
+                        'The participating are mainly students. ' +
+                        'In recent years, gatherings in cafes every Wednesday has become a form of ' +
+                        '"eating, drinking, talking about Python and sorts of things". ' +
+                        'New blood is needed to join.',
                 ],
             },
             {
                 tag: 'taichung',
                 title: 'Taichung - Taichung.py',
                 desc: [
-                    'Taichung.py was started in 2014 by Prof. Yuan-Liang Tang and several enthusiasts. The events were held at restaurants or coffee shops until later Microprogram Co. supported the event venue. Now the events are held monthly in Saturday afternoon, which is best suited for a walk to have some snacks in Fengjia Night Market after the event.',
+                    'Taichung.py was started in 2014 by Prof. Yuan-Liang Tang and ' +
+                        'several enthusiasts. The events were held at restaurants or coffee shops until ' +
+                        'later Microprogram Co. supported the event venue. ' +
+                        'Now the events are held monthly in Saturday afternoon, ' +
+                        'which is best suited for a walk to have some snacks in ' +
+                        'Fengjia Night Market after the event.',
                 ],
             },
             {
                 tag: 'tainan',
                 title: 'Tainan - Tainan.py',
                 desc: [
-                    'In 2013, Joe founded Tainan.py when he moved from Taipei to Tainan due to the change of company location. Since jserv also became the faculty in Department of Computer Science and Information Engineering, NCKU, Joe decided to launch “Tainan.py × MOSUT” events with MOSUT, another community located in Tainan. In order to promote local Tainan food, local dishes, like fried chicken and almond soup, are provided during the break in Tainan.py. Monthly meetup takes place in the weekend afternoon, so attendees are welcome to try more Tainan food after the meetup if the local dishes during the break is just not enough :)',
+                    'Joe, who moved to Tainan in 2013, founded Tainan.py. ' +
+                        'Decided to co-organize a gathering with the MOSUT community in Tainan' +
+                        ' to become "Tainan.py × MOSUT". ' +
+                        'The gathering is held once a month, usually on weekend afternoons. ' +
+                        'Tainan.py will promote local delicacies, ' +
+                        'so that everyone can enjoy the local flavors.',
                 ],
             },
             {
                 tag: 'kaohsiung',
                 title: 'Kaohsiung - Kaohsiung.py',
                 desc: [
-                    'Before Kaohsiung.py was founded, Python enthusiasts, like TooMore and CD, sometimes shared Python-related topics in the KSDG meetups. Founder of Kaohsiung.py is Victor Gau. He joined Tainan.py regularly until one day he had an accident with a speeding motorcycle on the way from Tainan to Kaohsiung. The accident induced him to launch a Python community in Kaohsiung. Therefore, Kaohsiung Python User Group was founded in 2014. In addition to casual meetups, which invites specialist from other cities, there is a regular gathering in Wenzao Ursuline University of Languages on one Monday night every month. They welcome anyone who passes by Kaohsiung to join the events.',
+                    'Before establishing Kaohsiung.py, ' +
+                        'Victor Gau participate in the Tainan.py event ' +
+                        'until he collided with a locomotive, ' +
+                        'giving Victor the idea of establishing a community in Kaohsiung. ' +
+                        'In 2014, Victor launched Kaohsiung.py. ' +
+                        'Arranging experts to share on Saturdays from time to time, ' +
+                        'he would pick one Monday every month to meet at Wenzao.',
                 ],
             },
             {
                 tag: 'hualien',
                 title: 'Hualien - Hualien.py',
                 desc: [
-                    'The history of Hualien.py can be dated back to 2013, Prof. Tzer-Jen Wei felt the lack of open source and technology communities in the east part of Taiwan. He started to invite Python speakers from other cities. Having speakers like c3h2, clkao and Mosky, Hualien.py began the first event in 2014. Events have been located in Hualien Railway Cultural Park, National Dong Hwa University, Hualien Cultural Creative Industries Park and so on. By the supports from all over the Taiwan, the community in Hualien thrives. Now there are more and more local speakers in the meetups. In addition to speeches, there are casual gathering and special focus workshop. Next time when having a trip in Taiwan east coast, drop by Hualien.py.                    ',
+                    'In 2013, Wei Zeren felt that there was a lack of open source in the east, ' +
+                        'and began to invite Python speakers from all over the world. ' +
+                        'With the willingness of Mosky, Hualien.py started smoothly in 2014. ' +
+                        'In the current form of gatherings, apart from speech sharing, ' +
+                        'there are also free gatherings and workshops on specific topics.',
                 ],
             },
             {
                 tag: 'nantou',
                 title: 'Nantou - Nantou.py',
                 desc: [
-                    'Led by the Vice President of Nan Kai University of Technology, Prof. Cheng-Min Lin, “Nantou.py” has regularly held a monthly meeting at 768 Art Space, a public welfare platform, to share the latest information and newest experiences with all the Python lovers in central Taiwan. Although Nantou County was once called the technology desert, our members have contributed their efforts to bring along industry connection as well as a selfless and boundless location or all the Python learners.',
+                    'Led by the Vice President of Nan Kai University of Technology, ' +
+                        'Prof. Cheng-Min Lin, Nantou.py has regularly held a monthly meeting at ' +
+                        '768 Art Space to share the latest information and newest experiences ' +
+                        'with all the Python lovers in central Taiwan. ' +
+                        'Members have contributed their efforts to bring along industry connection ' +
+                        'as well as a selfless and boundless location or all the Python learners.',
                 ],
             },
             {
                 tag: 'pyladiestw',
                 title: 'PyLadies Taiwan',
                 desc: [
-                    'PyLadies is a group of women developers who love the Python programming language. We are an international mentorship group with a focus on helping more women become active participants and leaders in the Python open-source community. We host monthly meetups with different topics such as beginners meetups, project of python presentation and tutorial. Open to all who identify as women. Feel free to join us!',
+                    'PyLadies is a group of women developers who love Python. ' +
+                        'We are an international mentorship group with a focus on helping more women ' +
+                        'become active participants and leaders in the Python community. ' +
+                        'We host monthly meetups with different topics, such as beginners meetups, ' +
+                        'project presentation, and tutorial. Open to all who identify as women. ' +
+                        'Feel free to join us!',
                 ],
             },
             {
                 tag: 'djangogirlstaipei',
                 title: 'Django Girls Taipei',
                 desc: [
-                    'Django Girls Taipei was founded by Michelle Leu in 2014. Currently there are casual tutorials and workshops to help girls build their own Django website. Also there are events held with the Python Web Meetup for novice. Highly recommend to girls with basic understanding of Django to learn more.',
+                    'Django Girls Taipei was founded by Michelle Leu in 2014. ' +
+                        'We held casual tutorials, workshops, meetups to help girls ' +
+                        'build their own Django website. ' +
+                        'Highly recommend to girls with basic understanding of ' +
+                        'Django to join us!.',
                 ],
             },
         ],
@@ -87,7 +129,7 @@ export default genI18nMessages({
             },
             {
                 tag: 'pyhug',
-                title: '新竹 - PyHug',
+                title: '新竹 - PyHUG',
                 desc: [
                     '台灣第一個 Python 社群，2011 年由 yyc 和 Albert Huang 成立。每月定期邀請講者分享。參加成員以大學生為主，聚會偏重Python 在科學上的應用。',
                     '近幾年每週三在咖啡館聚會，漸漸變成「吃吃喝喝聊 Python 及雜七雜八」的形式。目前由 Dainese 擔任負責人，需要地方的新血加入。',
@@ -113,7 +155,7 @@ export default genI18nMessages({
                 tag: 'kaohsiung',
                 title: '高雄 - Kaohsiung.py',
                 desc: [
-                    'Victor Gau在成立 Kaohsiung.py 之前，定期會去參加 Joe 安排的 Tainan.py 活動，直到有一次回程時，與超速的機車擦撞，讓 Victor 萌生在高雄成立社群的念頭。',
+                    'Victor Gau 在成立 Kaohsiung.py 之前，定期會去參加 Joe 安排的 Tainan.py 活動，直到有一次回程時，與超速的機車擦撞，讓 Victor 萌生在高雄成立社群的念頭。',
                     '2014 年，Victor 在高雄發起了 Kaohsiung.py ，除了不定期的在禮拜六安排專家來分享，每個月會挑一個星期一，在文藻外語大學聚會。',
                 ],
             },
@@ -129,7 +171,7 @@ export default genI18nMessages({
                 tag: 'nantou',
                 title: '南投 - Nantou.py',
                 desc: [
-                    '2019年底，南開科技大學副校長林正敏教授，創立了南投.py 。聚會地點在草屯768藝術空間，固定每月舉行分享會，來自各地的大夥也無私地向大家分享技術。',
+                    '2019年底，南開科技大學副校長林正敏教授，創立了 Nantou.py。聚會地點在草屯768藝術空間，固定每月舉行分享會，來自各地的大夥也無私地向大家分享技術。',
                     '在南投人稱科技沙漠的地區，有著這一群人為python貢獻己力，並為有意學習的大家，帶來一場無私、無限的學習地。',
                 ],
             },
@@ -138,7 +180,7 @@ export default genI18nMessages({
                 title: 'PyLadies Taiwan',
                 desc: [
                     'PyLadies 是一個專屬於女生的 Python 愛好者聚會，想瞭解 Python 的女生彼此交流、聊天認識朋友的地方。',
-                    '透過Workshop的方式讓女生成為主動的參與者。每個月舉辦不同主題的活動，例：初學者的入門、Python 的應用與經驗分享。只要對 Python 有興趣，歡迎妳來參加喔！',
+                    '透過 Workshop 的方式讓女生成為主動的參與者。每個月舉辦不同主題的活動，例：初學者的入門、Python 的應用與經驗分享。只要對 Python 有興趣，歡迎妳來參加喔！',
                 ],
             },
             {
