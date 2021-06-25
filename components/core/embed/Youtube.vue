9cb3b941d87c2cdd88bbbddb0057f2fbabaf6a2b
Matt.Wang <mattwang44@gmail.com>
2021-06-26 06:46:28 +0800

    feat: add component for embedded YouTube video

---
+++ components/core/embed/Youtube.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="video-container">
+        <iframe
+            :src="`https://www.youtube.com/embed/${videoId}`"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        >
+        </iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        videoId: { type: String, required: true },
+    },
+}
+</script>
+
+<style lang="postcss" scoped>
+.video-container {
+    @apply aspect-w-16 aspect-h-9;
+}
+</style>
