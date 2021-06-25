97bf16fb022995b848dc7f3f3020a7cad8cd4f02
Matt.Wang <mattwang44@gmail.com>
2021-06-26 06:48:34 +0800

    feat: add component for tabs

---
+++ components/core/tabs/Tab.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-show="isActive" class="tab">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'Tab',
+        },
+    },
+    data() {
+        return {
+            isActive: true,
+        }
+    },
+}
+</script>
---
+++ components/core/tabs/Tabs.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <ul class="tabs__headers">
+            <li
+                v-for="(tab, index) in tabs"
+                :key="tab.title"
+                :class="{ header: true, '-selected': isSelectedIndex(index) }"
+                @click="selectTab(index)"
+            >
+                {{ tab.title }}
+            </li>
+        </ul>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedIndex: 0,
+            tabs: [],
+        }
+    },
+    computed: {},
+    created() {
+        this.tabs = this.$children
+    },
+    mounted() {
+        this.selectTab(0)
+    },
+    methods: {
+        selectTab(i) {
+            this.selectedIndex = i
+            this.tabs.forEach((tab, index) => {
+                tab.isActive = index === i
+            })
+        },
+        isSelectedIndex(i) {
+            return i === this.selectedIndex
+        },
+    },
+}
+</script>
+
+<style lang="postcss" scoped>
+.tabs__headers {
+    @apply ml-4;
+}
+.tabs__headers > .header {
+    @apply inline-block ml-1 py-2 px-4 border-2 rounded-t-lg bg-transparent text-sm text-center font-bold cursor-pointer;
+    color: #e6ba17;
+    border-color: #c2a53a;
+    font-family: 'Noto Serif TC';
+    min-width: 112px;
+}
+.tabs__headers > .header.-selected {
+    background: #e6ba17;
+    color: #121023;
+}
+
+.tab {
+    @apply inline-block text-sm rounded-2xl min-w-full;
+    color: #282828;
+    background: #e6ba17;
+    border-color: #c2a53a;
+    padding: 20px;
+}
+</style>
