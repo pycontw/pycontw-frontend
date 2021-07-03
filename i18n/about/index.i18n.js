622e8e82e38adb2b1baa8f6761aacb206ec98668
Josix <josixwang@gmail.com>
2021-07-03 23:28:10 +0800

    fix(About): Fix padding b/w paragraph and wrong titles mentioned in #75 and #77

--- i18n/about/index.i18n.js
+++ i18n/about/index.i18n.js
@@ -2,7 +2,7 @@ import { genI18nMessages } from '@/utils/i18n.utils'
 
 export default genI18nMessages({
     'en-us': {
-        title: 'About PyCon Taiwan',
+        title: 'About',
         pageAbstract: [
             'PyCon, also known as the Python Annual Conference, is the annual event of the global Python community.',
             'PyCon is spontaneously organized by fellow locals, and now Taiwan has its own Python annual conference. This event aims to gather people who use Python in various fields in Taiwan to exchange their experiences, share the latest technological developments, and experience the unique enthusiasm of the community.',
@@ -32,7 +32,7 @@ export default genI18nMessages({
         },
     },
     'zh-hant': {
-        title: '關於 PyCon Taiwan',
+        title: '關於',
         pageAbstract: [
             'PyCon ，亦即 Python 年會，是全球 Python 社群年度盛會。',
             'PyCon 由各地同好自發籌辦，而現在台灣也有自己的 Python 年會了。這個活動旨在聚集台灣各領域應用 Python 的人，交流彼此的經驗心得，分享最新的技術發展，並親身體驗社群獨有的熱情。',
