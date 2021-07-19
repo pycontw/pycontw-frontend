71d1890d90b6e08c2fe6287431695e80e8781838
Matt.Wang <mattwang44@gmail.com>
2021-07-19 19:58:02 +0800

    chore: retrieve API token with http plugin

--- store/index.js
+++ store/index.js
@@ -6,8 +6,6 @@ export const state = () => ({
     keynotesData: [],
 })
 
-const token = process.env.AUTH_TOKEN
-
 export const mutations = {
     setSponsorsData: set('sponsorsData'),
     setJobsData: set('jobsData'),
@@ -16,21 +14,15 @@ export const mutations = {
 
 export const actions = {
     async $getSponsorsData({ commit }) {
-        const { data } = await this.$http.$get('/api/sponsors', {
-            headers: { Authorization: token },
-        })
+        const { data } = await this.$http.$get('/api/sponsors')
         commit('setSponsorsData', data)
     },
     async $getJobsData({ commit }) {
-        const { data } = await this.$http.$get('/api/sponsors/jobs', {
-            headers: { Authorization: token },
-        })
+        const { data } = await this.$http.$get('/api/sponsors/jobs')
         commit('setJobsData', data)
     },
     async $getKeynotesData({ commit }) {
-        const keynoteList = await this.$http.$get('/api/events/keynotes', {
-            headers: { Authorization: token },
-        })
+        const keynoteList = await this.$http.$get('/api/events/keynotes')
         commit('setKeynotesData', keynoteList)
     },
 }
