c4a6238c507cb9264403d083c5b8a35dfb158ad9
Matt.Wang <mattwang44@gmail.com>
2021-07-30 22:38:07 +0800

    chore: add og:description content for ext/live page

--- i18n/ext/live.i18n.js
+++ i18n/ext/live.i18n.js
@@ -5,20 +5,20 @@ export default genI18nMessages({
         invalidAttendee:
             'The token within the link is invalid. Please contact staff for further help.',
         title: 'Live Broadcast',
-        pageAbstract: '',
+        pageAbstract: 'Live Broadcast',
         og: {
             title: 'Live Broadcast',
-            pageAbstract: '',
+            pageAbstract: 'Live Broadcast',
         },
     },
     'zh-hant': {
         invalidAttendee:
             '連結中附帶的驗證碼錯誤。請聯繫工作人員取得更進一步的協助。',
         title: '現場直播',
-        pageAbstract: '',
+        pageAbstract: '現場直播',
         og: {
             title: '現場直播',
-            description: '',
+            description: '現場直播',
         },
     },
 })
