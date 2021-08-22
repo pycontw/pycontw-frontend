3de6f3be122f0f9df1ed85784c9233383877abf7
yuyanghh <fisheepx@gmail.com>
2021-08-22 22:59:26 +0800

    add new props to core h1

--- components/core/layout/Hr.vue
+++ components/core/layout/Hr.vue
@@ -1,17 +1,32 @@
 <template>
-    <div class="hr"></div>
+    <div
+        class="hr"
+        :style="{
+            margin: `${marginVertical}px auto`,
+            borderColor: color,
+        }"
+    ></div>
 </template>
 
 <script>
 export default {
     name: 'CoreHr',
+    props: {
+        color: {
+            type: String,
+            default: '#868686',
+        },
+        marginVertical: {
+            type: Number,
+            default: 40,
+        },
+    },
 }
 </script>
 
 <style scoped>
 .hr {
     width: 100%;
-    border: 1px solid #868686;
-    margin: 40px auto;
+    border: 1px solid;
 }
 </style>
