5bbbe87a52925366af3f8755f1d2962f2df7f64b
Set Mao <set.mao.tw@gmail.com>
2021-12-04 23:43:00 +0800

    feat(i18n): add join us string in i18n

--- i18n/index.i18n.js
+++ i18n/index.i18n.js
@@ -8,6 +8,7 @@ export default genI18nMessages({
         sponsorList: 'Sponsors',
         sponsorUs: 'Be a Sponsor',
         bulletinList: 'Latest Announcement',
+        joinUs: 'Join Us',
     },
     'zh-hant': {
         pyconWelcome: '歡迎來到 PyCon TW 2021',
@@ -16,5 +17,6 @@ export default genI18nMessages({
         sponsorList: '贊助夥伴',
         sponsorUs: '成為贊助夥伴',
         bulletinList: '最新公告',
+        joinUs: '成為志工',
     },
 })
