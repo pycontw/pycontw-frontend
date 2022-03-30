896c621248575f18d69c0106dab0a9672a8b36c0
Matt.Wang <mattwang44@gmail.com>
2022-03-30 13:39:54 +0800

    chore: update og:description for sponsor page

--- i18n/sponsor/index.i18n.js
+++ i18n/sponsor/index.i18n.js
@@ -29,11 +29,11 @@ export default genI18nMessages({
             link: 'https://docs.google.com/forms/d/1b-SHv1JO_8gyPe3385l-3y78qg4njmV7MLFIL4VxIPg/',
         },
         og: {
-            title: 'Sponsor page',
+            title: 'Sponsorship',
             description:
-                'PyCon Taiwan is driven by our members and, YOU, our awesome sponsor partners! ' +
-                'Your generous support is the helping us to move forward.' +
-                'PyCon Taiwan calls for your sponsorship in order to enlarging our community.',
+                'PyCon Taiwan is driven by our members and you, our awesome sponsor partners! ' +
+                'Your generous support is the motivation to help us move forward. ' +
+                'PyCon APAC 2022 calls for your sponsorship to enlarge our community.',
         },
     },
     'zh-hant': {
@@ -63,8 +63,8 @@ export default genI18nMessages({
         og: {
             title: '贊助召集令',
             description:
-                '「您」的支持是推進 PyCon Taiwan 的強大動力！' +
-                'PyCon Taiwan 在此召集「您」的加入，透過贊助以具體行動支持 python 社群的成長！',
+                '您的支持是推進 PyCon Taiwan 的強大動力。' +
+                'PyCon Taiwan 在此召集您的加入，透過贊助以具體行動支持 Python 社群的成長！',
         },
     },
 })
