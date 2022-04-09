929d36239f46a05982c4689d4ec915374dca5d19
Matt.Wang <mattwang44@gmail.com>
2022-04-10 07:45:06 +0800

    feat(registration): revamp ticket info page

--- i18n/registration/tickets.i18n.js
+++ i18n/registration/tickets.i18n.js
@@ -2,145 +2,53 @@ import { genI18nMessages } from '@/utils/i18n.utils'
 
 export default genI18nMessages({
     'en-us': {
-        title: 'Online Ticket',
+        title: '探索無疆界宇宙，挑戰號準備啟程。',
         pageAbstract:
-            'Welcome to PyCon TW. You can purchase different ticket types, ' +
-            'and find the most suitable plan. If you need financial assistance, ' +
-            'please read the financial aid section below. ',
-        individualTicket: {
-            title: 'Individual',
-            summary:
-                'Tickets are limited. Be sure to get your ticket as soon as possible before they are sold out. ' +
-                'We will send out electronic invoices by email after the conference. ' +
-                '{highlight} All ticket sales end on September 4th.',
-            pySafe: 'The souvenirs of PySafe include stickers, Taiwanese beer glass, T-shirt, and thankful card.',
-            highlight: 'We do not provide VAT receipt for individual tickets. ',
-            type: [
-                {
-                    name: 'Regular',
-                    discount: 'Regular',
-                },
-                {
-                    name: 'Regular (with PySafe)',
-                    discount: 'Regular',
-                },
-            ],
+            '多樣票價供您選擇，人人都可以找到適合自己的方案。' +
+            '多元化學習與多視角觀察技術應用，深入淺出引你進入 Python 學習殿堂。' +
+            '跨越地球距離阻礙，串連亞太地區，與您一同揭露不可能的可能秘密。',
+        introTitle: '購票方案簡介',
+        introContent:
+            '從早鳥票到一般票，企業票至個人贊助票，您可以依照您的需求選擇您需要的票種，' +
+            '若需要財務補助的你，請至{fa}進行申請。{br}' +
+            '限量大會紀念品『Pyckage』，即將登場。',
+        features: {
+            onSales: '35% off Discount',
+            limited100: 'Limited edition tickets',
+            pyckage: 'Pyckage delivered to address anywhere in the world',
+            conference: 'Conference pass',
+            vatAvailable: 'VAT receipt provided',
         },
-        corporateTicket: {
-            title: 'Corporate',
-            summary: [
-                'Provide company name and VAT Number, ' +
-                    'or N/A if re­im­burse­ment is not required. ' +
-                    'We will send out electronic invoices by email after the conference. ',
-                '{discount}',
-                'The sales of corporate tickets ended on September 4th.',
-            ],
-            pySafe: 'The souvenirs of PySafe include stickers, Taiwanese beer glass, T-shirt and thankful card.',
-            type: {
-                name: 'Corporate (with PySafe)',
-                discount: 'Regular',
-            },
-            discount:
-                'We offer discounts for groups of five or more people. ' +
-                'For group purchase, please fill out the {formLink}! ',
-            formLink: 'Google form',
-        },
-        notices: {
-            title: 'Notices',
-            description: [
-                '{reimbursement}',
-                'Tickets provide access to the main conference from Oct. 2nd to Oct. 3rd. ' +
-                    'Events like Tutorials or Sprints might need a separate registration. ' +
-                    "Be sure to check out the related details on the event's page.",
-                'Please provide the valid phone number and email address when purchasing the ticket, ' +
-                    "so that you won't be missed if we have important announcements. " +
-                    'If you have any questions, please send us an e-mail to ' +
-                    "{organizers}, and we'll reply as soon as possible.",
-                'PyCon Taiwan is a paid conference. Everybody, including staffs and speakers, ' +
-                    'contributes or pays for attending (see PyCon: {everybodyPays}).',
-                'You can pay through credit card, PayPal, or bank transfer.',
-                'You may ask for a refund not later than ten days before the conference, ' +
-                    'by contacting us or directly applying with KKTIX.',
-            ],
-            reimbursement:
-                'If you need reimbursement, please purchase the corporate ticket. ' +
-                'VAT receipts are not provided for other ticket types.',
-            everybodyPays: 'Everybody Pays',
-        },
-        financialAid: {
-            title: 'Financial Aid',
-            description:
-                'All who look for help attending the conference are welcomed ' +
-                'to apply for financial aid. {highlight}. ' +
-                'More info about applications can be found {financialAidLink}.',
-            financialAidLink: 'here',
-            highlight: 'Please apply before your purchase',
-        },
-        ticketLinkText: 'Buy Ticket',
+        ticketEarlyBird: 'Early Bird Pass',
+        ticketRegular: 'Individual Pass',
+        ticketEnterprise: 'Corporate Pass',
+        buttonText: '立即搶購',
+        buttonDisabledText: '即將開賣',
+        fa: 'financial aid',
     },
     'zh-hant': {
-        title: '購票',
+        title: '探索無疆界宇宙，挑戰號準備啟程。',
         pageAbstract:
-            '歡迎大家參與 PyCon 精彩的議程。不同的身份可以購買不同的票種，' +
-            '每個人都可以找到最適合自己的方案，有興趣的會眾請儘早購買！ ' +
-            '若需要財務補助，請見頁底財務補助申請。',
-        individualTicket: {
-            title: '個人票',
-            summary:
-                '限時限量，請及早購買。電子發票於會後統一以 email 寄送，' +
-                '{highlight} 一般票種販售於 9 月 4 日結束。',
-            pySafe: 'PySafe 內容物包含紀念貼紙、紀念酒杯、紀念 T-shirt以及感謝小卡。',
-            highlight: '個人票一律無法使用統一編號。',
-            type: [
-                {
-                    name: '一般票',
-                    discount: '一般價',
-                },
-                {
-                    name: '一般票（附 PySafe）',
-                    discount: '一般價',
-                },
-            ],
+            '多樣票價供您選擇，人人都可以找到適合自己的方案。' +
+            '多元化學習與多視角觀察技術應用，深入淺出引你進入 Python 學習殿堂。' +
+            '跨越地球距離阻礙，串連亞太地區，與您一同揭露不可能的可能秘密。',
+        introTitle: '購票方案簡介',
+        introContent:
+            '從早鳥票到一般票，企業票至個人贊助票，您可以依照您的需求選擇您需要的票種，' +
+            '若需要財務補助的你，請至{fa}進行申請。{br}' +
+            '限量大會紀念品『Pyckage』，即將登場。',
+        features: {
+            onSales: '限時 65 折優惠',
+            limited100: '限量票 100 張',
+            pyckage: '限量大會紀念品 Pyckage',
+            conference: '全程參與 PyCon APAC 2022',
+            vatAvailable: '可使用統一編號開發票',
         },
-        corporateTicket: {
-            title: '企業票',
-            summary: [
-                '有統編需求請填寫公司抬頭與統一編號。無統編需求請於抬頭與統編填寫 N/A。' +
-                    '企業票的電子發票標示買方統編可用於公司報帳。電子發票於會後統一以 email 寄送。',
-                '{discount}',
-                '企業票販售於 9 月 4 日結束。',
-            ],
-            pySafe: 'PySafe 內容物包含紀念貼紙、紀念酒杯、紀念 T- shirt 以及感謝小卡。',
-            type: {
-                name: '企業票（附 PySafe）',
-                discount: '一般價',
-            },
-            discount: '若五人以上購買提供團購優惠，如需團購請填寫{formLink}！',
-            formLink: '表單',
-        },
-        notices: {
-            title: '購票須知',
-            description: [
-                '{reimbursement}',
-                '上列票種用於報名 10 月 2 日至 3 日的議程活動；tutorial、衝刺開發等其他課程報名方式，請至首頁報名。',
-                '報名時請留下正確的聯絡方式以便通知。如有問題請寄信至 {organizers} 洽詢，我們將盡快回應。',
-                'PyCon Taiwan 是需付費的活動。所有參加者，包括籌備人員和講者，' +
-                    '均需透過貢獻 PyCon Taiwan 社群或是付費而來參加。見{everybodyPays}。',
-                '我們提供三種付款方式：信用卡、PayPal、ATM 轉帳。',
-                '部份票種（例如個人票）可由 KKTIX 逕行辦理退票，您可於會議十天前聯絡我們。',
-            ],
-            reimbursement:
-                '如果需要抬頭和統編，請購買企業票，其他票種無法提供統編。',
-            everybodyPays: '公平付費',
-        },
-        financialAid: {
-            title: '財務補助',
-            description:
-                '歡迎申請大會 Financial Aid 計劃。{highlight}，' +
-                '再依獲得的憑證購買補助用票券。若有意願申請請至{financialAidLink}。',
-            financialAidLink: '財務補助說明頁面',
-            highlight: '請於購票之前先行申請',
-        },
-        ticketLinkText: '線上購票',
+        ticketEarlyBird: '早鳥票',
+        ticketRegular: '一般票',
+        ticketEnterprise: '企業票',
+        buttonText: '立即搶購',
+        buttonDisabledText: '即將開賣',
+        fa: '財務補助申請',
     },
 })
--- pages/registration/tickets.vue
+++ pages/registration/tickets.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tickets py-10 md:py-24 lg:py24">
+    <div class="py-10 md:py-24 lg:py24">
         <banner>
             <template #default>
                 <div
@@ -14,185 +14,82 @@
                 </p>
             </template>
         </banner>
+
         <i18n-page-wrapper class="i18n__custom pt-8 px-8 md:px-57 lg:px-56">
-            <div class="tickets__individual">
-                <h3>{{ $t('individualTicket.title') }}</h3>
-                <div class="tickets__summary">
-                    <i18n path="individualTicket.summary" tag="p">
-                        <template #highlight>
-                            <span class="customHighlight">
-                                {{ $t('individualTicket.highlight') }}
-                            </span>
-                        </template>
-                    </i18n>
-                    <p>{{ $t('individualTicket.pySafe') }}</p>
-                </div>
-                <div class="tickets__boxWrapper">
-                    <div
-                        v-for="(individual, index) in $t(
-                            'individualTicket.type',
-                        )"
-                        :key="`ticket_individual_type_${index}`"
-                        class="tickets__box"
-                    >
-                        <h5>{{ individual.name }}</h5>
-                        <div class="tickets__boxPrice">
-                            <div>
-                                <p>{{ individual.discount }}</p>
-                            </div>
-                            <div class="flex">
-                                <p class="tickets__boxPriceHighlight">
-                                    {{ individualPrice[index] }}
-                                </p>
-                                <p>NTD</p>
-                            </div>
-                        </div>
-                        <text-button :href="links.individual" secondary>{{
-                            $t('ticketLinkText')
-                        }}</text-button>
-                    </div>
-                </div>
-                <img
-                    class="tickets__individualCircle"
-                    src="~/static/img/registration/TicketsRightCircle.svg"
-                    alt="PyConTW Tickets Page Circle Icon"
-                />
-            </div>
-            <div class="tickets__corporate">
-                <h3>{{ $t('corporateTicket.title') }}</h3>
-                <div class="tickets__summary">
-                    <p>
-                        <i18n
-                            v-for="(text, index) in $t(
-                                'corporateTicket.summary',
-                            )"
-                            :key="`ticket_corporate_summary_${index}`"
-                            :path="`corporateTicket.summary.${index}`"
-                            tag="span"
+            <!-- intro -->
+            <div class="pb-12">
+                <h2>{{ $t('introTitle') }}</h2>
+                <i18n path="introContent" tag="p" class="introContent">
+                    <template #fa>
+                        <locale-link
+                            to="/registration/financial-aid"
+                            underline
+                            highlight
+                            >{{ $t('fa') }}</locale-link
                         >
-                            <template #discount>
-                                <i18n
-                                    path="corporateTicket.discount"
-                                    tag="span"
-                                    class="customHighlight"
-                                >
-                                    <template #formLink>
-                                        <ext-link
-                                            :href="links.corporateGroup"
-                                            underline
-                                            >{{
-                                                $t('corporateTicket.formLink')
-                                            }}</ext-link
-                                        >
-                                    </template>
-                                </i18n>
-                            </template>
-                        </i18n>
-                    </p>
-                    <p>{{ $t('corporateTicket.pySafe') }}</p>
-                </div>
-                <div class="tickets__boxWrapper">
-                    <div class="tickets__box">
-                        <h5>{{ $t('corporateTicket.type.name') }}</h5>
-                        <div class="tickets__boxPrice">
-                            <div>
-                                <p>{{ $t('corporateTicket.type.discount') }}</p>
-                            </div>
-                            <div class="flex">
-                                <p class="tickets__boxPriceHighlight">
-                                    {{ corporatePrice }}
-                                </p>
-                                <p>NTD</p>
-                            </div>
+                    </template>
+                    <template #br><br /></template>
+                </i18n>
+            </div>
+
+            <!-- ticket plans -->
+            <div class="tickets">
+                <div
+                    v-for="(ticketInfo, index) in ticketInfos"
+                    :key="`ticket_info_${index}`"
+                    class="ticketContainer"
+                >
+                    <div class="tickerHeader">
+                        <img
+                            :src="ticketInfo.image"
+                            :alt="ticketInfo.tag"
+                            class="icon"
+                        />
+                        <div class="title">
+                            {{ $t(ticketInfo.titleI18NPath) }}
                         </div>
-                        <text-button :href="links.corporate" secondary>{{
-                            $t('ticketLinkText')
+                    </div>
+                    <ul class="features">
+                        <li
+                            v-for="(feature, i) in ticketInfo.features"
+                            :key="`ticket_info_${ticketInfo.tag}_features_${i}`"
+                            class="feature"
+                        >
+                            ✓ {{ $t(`features.${feature}`) }}
+                        </li>
+                    </ul>
+                    <div class="price">
+                        <div v-if="ticketInfo.priceOnSale" class="priceOnSale">
+                            {{ ticketInfo.priceOnSale }}
+                        </div>
+                        <div :class="isStrikethrough(!!ticketInfo.priceOnSale)">
+                            {{ ticketInfo.price }}
+                        </div>
+                    </div>
+                    <div class="button">
+                        <text-button
+                            v-if="ticketInfo.link"
+                            small
+                            :href="ticketInfo.link"
+                            >{{ $t('buttonText') }}</text-button
+                        >
+                        <text-button v-else small>{{
+                            $t('buttonDisabledText')
                         }}</text-button>
                     </div>
                 </div>
-                <img
-                    class="tickets__corporateCircle"
-                    src="~/static/img/registration/TicketsLeftCircle.svg"
-                    alt="PyConTW Tickets Page Circle Icon"
-                />
-            </div>
-            <div class="tickets__notices">
-                <h2>{{ $t('notices.title') }}</h2>
-                <ul class="list-disc">
-                    <i18n
-                        v-for="(notice, index) in $t('notices.description')"
-                        :key="`ticket_notice_${index}`"
-                        :path="`notices.description.${index}`"
-                        tag="li"
-                    >
-                        <template #reimbursement>
-                            <li class="customHighlight">
-                                {{ $t('notices.reimbursement') }}
-                            </li>
-                        </template>
-                        <template #everybodyPays>
-                            <ext-link
-                                :href="links.everybodyPays"
-                                highlight
-                                underline
-                                >{{ $t('notices.everybodyPays') }}</ext-link
-                            >
-                        </template>
-                        <template #organizers>
-                            <ext-link
-                                href="mailto:organizers＠pycon.tw"
-                                highlight
-                                underline
-                                >organizers＠pycon.tw</ext-link
-                            >
-                        </template>
-                    </i18n>
-                </ul>
-                <img
-                    class="tickets__curveDecorate"
-                    src="~/static/img/registration/TicketsCurve.svg"
-                    alt="PyConTW Tickets Page Curve Icon"
-                />
-                <img
-                    class="tickets__noticesCircle"
-                    src="~/static/img/registration/TicketsRightCircle.svg"
-                    alt="PyConTW Tickets Page Circle Icon"
-                />
-            </div>
-            <div class="tickets__financialAid">
-                <h2>{{ $t('financialAid.title') }}</h2>
-                <ul class="list-disc">
-                    <i18n path="financialAid.description" tag="li">
-                        <template #highlight>
-                            <span class="customHighlight">{{
-                                $t('financialAid.highlight')
-                            }}</span>
-                        </template>
-                        <template #financialAidLink>
-                            <locale-link
-                                to="/registration/financial-aid"
-                                highlight
-                                class="underline"
-                                >{{
-                                    $t('financialAid.financialAidLink')
-                                }}</locale-link
-                            >
-                        </template>
-                    </i18n>
-                </ul>
             </div>
         </i18n-page-wrapper>
     </div>
 </template>
 
 <script>
-import I18nPageWrapper from '@/components/core/i18n/PageWrapper'
 import i18n from '@/i18n/registration/tickets.i18n'
+
+import I18nPageWrapper from '@/components/core/i18n/PageWrapper'
 import CoreH1 from '@/components/core/titles/H1'
 import Banner from '@/components/core/layout/Banner'
-import ExtLink from '@/components/core/links/ExtLink.vue'
-import LocaleLink from '@/components/core/links/LocaleLink.vue'
-import TicketsBanner from '@/static/img/about/Banner.svg'
+import { LocaleLink } from '@/components/core/links'
 import TextButton from '@/components/core/buttons/TextButton'
 
 export default {
@@ -202,22 +99,48 @@ export default {
         CoreH1,
         I18nPageWrapper,
         Banner,
-        ExtLink,
+
         LocaleLink,
         TextButton,
     },
     data() {
         return {
-            ticketsBanner: TicketsBanner,
-            individualPrice: ['1,500', '1,800'],
-            corporatePrice: '3,000',
-            links: {
-                individual: 'https://pycontw.kktix.cc/events/2021-individual',
-                corporate: 'https://pycontw.kktix.cc/events/2021-corporate',
-                corporateGroup: 'https://forms.gle/3DRBk24HBZLMsV7w6',
-                everybodyPays:
-                    'http://jessenoller.com/blog/2011/05/25/pycon-everybody-pays',
-            },
+            ticketsBanner: require('~/static/img/about/Banner.svg'),
+            ticketInfos: [
+                {
+                    tag: 'earlyBird',
+                    titleI18NPath: 'ticketEarlyBird',
+                    priceOnSale: 'NT$ 1,690',
+                    price: 'NT$ 2,600',
+                    image: require('~/static/img/registration/tickets/earlyBird.svg'),
+
+                    // The strings in this array are the tags listed in `features` object
+                    // of i18n file (~i18n/registration/tickets.i18n.js).
+                    features: [
+                        'onSales',
+                        'limited100',
+                        'pyckage',
+                        'conference',
+                    ],
+                    link: 'https://pycontw.kktix.cc',
+                },
+                {
+                    tag: 'regular',
+                    titleI18NPath: 'ticketRegular',
+                    price: 'NT$ 2,600',
+                    image: require('~/static/img/registration/tickets/regular_disabled.svg'),
+                    features: ['pyckage', 'conference'],
+                    link: null,
+                },
+                {
+                    tag: 'enterprise',
+                    titleI18NPath: 'ticketEnterprise',
+                    price: 'NT$ 3,600',
+                    image: require('~/static/img/registration/tickets/enterprise_disabled.svg'),
+                    features: ['vatAvailable', 'pyckage', 'conference'],
+                    link: null,
+                },
+            ],
         }
     },
     computed: {
@@ -227,6 +150,11 @@ export default {
                 'background-repeat': 'no-repeat',
                 'background-position': 'center',
             }
+        },
+    },
+    methods: {
+        isStrikethrough(indicator) {
+            return indicator ? { 'line-through': true } : {}
         },
     },
     head() {
@@ -255,94 +183,85 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.customHighlight {
-    color: #9387ff;
-}
-
-.tickets h2 {
-    @apply font-serif font-bold text-center;
-    @apply mt-14 mb-10 text-xl md:text-2xl tracking-widest;
-    color: #c386ae;
-}
-
-.tickets h3 {
-    @apply font-serif font-bold text-base md:text-xl;
-}
-
-.tickets h5 {
-    @apply text-center text-base sm:text-lg md:text-xl;
-    @apply font-bold mt-6 md:mt-8;
-    color: #c386ae;
-}
-
-.tickets span {
-    @apply text-xs md:text-base;
-}
-
-.tickets ul.list-disc {
-    @apply pl-4 md:pl-14;
-}
-
-.tickets__summary {
-    @apply pb-9;
-}
-
-.tickets__summary p {
-    @apply text-xs md:text-base my-0;
-}
-
-.tickets .tickets__box {
-    @apply text-center box-border rounded-3xl h-56 md:h-72 z-10;
-    border: 2px solid #c386ae;
-    box-shadow: 6px 6px 0px #c386ae;
-}
-
-.tickets__boxWrapper {
-    @apply grid md:grid-cols-2 gap-10;
-}
-
-.tickets__boxPrice {
-    @apply flex justify-evenly mt-8 mb-3 md:mt-10 md:mb-6;
-    @apply text-xs sm:text-sm md:text-base;
-    line-height: 2.4rem;
-    @media (min-width: 768px) {
-        line-height: 2.85rem;
+.introContent {
+    @apply leading-5;
+    font-size: 16px;
+    @media (min-width: 1280px) {
+        font-size: 18px;
     }
 }
 
-.tickets__boxPrice .tickets__boxPriceHighlight {
-    @apply font-bold mr-1 text-2xl md:text-3xl;
+.tickets {
+    @apply w-full;
+}
+
+.ticketContainer {
+    @apply flex flex-col xl:flex-row justify-around;
+    @apply py-10 xl:py-8;
+}
+
+.ticketContainer .tickerHeader {
+    @apply my-auto;
+    padding: 0 0 24px;
+    @media (min-width: 1280px) {
+        padding: 0;
+    }
+}
+.ticketContainer .icon {
+    @apply w-20 mx-auto;
+    padding: 0 0 12px;
+}
+.ticketContainer .title {
+    @apply font-serif font-semibold text-center;
+    font-size: 22px;
+    @media (min-width: 1280px) {
+        font-size: 32px;
+    }
+}
+
+.ticketContainer .features {
+    @apply flex flex-col justify-center mx-auto;
+    margin-bottom: 0 !important;
+    padding: 0 0 24px;
+    @media (min-width: 1280px) {
+        padding: 0;
+    }
+}
+.ticketContainer .feature {
+    @apply font-sans;
+    font-size: 16px;
+    @media (min-width: 1280px) {
+        font-size: 18px;
+    }
+}
+
+.ticketContainer .price {
+    @apply flex flex-row-reverse xl:flex-col;
+    @apply text-center m-auto;
+    padding: 0 0 24px;
+    font-size: 22px;
+    @media (min-width: 1280px) {
+        padding: 0;
+        font-size: 28px;
+    }
+}
+.ticketContainer .priceOnSale {
+    @apply font-bold text-center mx-1.5 my-auto;
     color: #c386ae;
 }
 
-.tickets__notices li,
-.tickets__financialAid li {
-    @apply mb-2 text-xs md:text-base;
+.ticketContainer .button {
+    @apply flex items-center mx-auto;
 }
 
-.tickets__individualCircle {
-    @apply hidden absolute md:block md:right-2 lg:right-12 z-0;
-    top: 40rem;
-}
+h2 {
+    @apply font-serif font-bold text-center pb-4 pt-8 xl:pb-10 xl:pt-14;
+    @apply mt-0 mb-2 tracking-widest;
+    font-size: 28px;
+    color: #c386ae;
 
-.tickets__noticesCircle {
-    @apply hidden absolute md:block md:right-2 lg:right-12 z-0;
-    top: 107.5rem;
-}
-
-.tickets__corporateCircle {
-    @apply hidden absolute md:block md:left-2 lg:left-10 z-0;
-    top: 54rem;
-}
-
-.tickets__curveDecorate {
-    @apply hidden absolute md:block md:left-0;
-    z-index: -1;
-    @media (min-width: 768px) {
-        top: 78rem;
-    }
-    @media (min-width: 1440px) {
-        top: 62rem;
+    @media (min-width: 1280px) {
+        font-size: 32px;
     }
 }
 </style>
