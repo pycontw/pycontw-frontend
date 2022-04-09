9afc91f8a0f4b5707ffb396fc714dd1bb728bc46
Matt.Wang <mattwang44@gmail.com>
2022-04-10 05:19:22 +0800

    fix(design-system): button styling

--- components/core/buttons/TextButton.vue
+++ components/core/buttons/TextButton.vue
@@ -1,13 +1,15 @@
 <template>
-    <button :class="coreButtonClasses">
-        <ext-link v-if="href" :href="href">
-            <slot></slot>
-        </ext-link>
-        <locale-link v-else-if="to" :to="to" customized>
-            <slot></slot>
-        </locale-link>
-        <slot v-else></slot>
-    </button>
+    <div>
+        <button :class="coreButtonClasses">
+            <ext-link v-if="href" :href="href">
+                <span><slot></slot></span>
+            </ext-link>
+            <locale-link v-else-if="to" :to="to" customized>
+                <span><slot></slot></span>
+            </locale-link>
+            <slot v-else></slot>
+        </button>
+    </div>
 </template>
 
 <script>
@@ -54,7 +56,7 @@ export default {
         },
         uppercase: {
             type: Boolean,
-            default: true,
+            default: false,
         },
         bulletin: {
             type: Boolean,
@@ -88,43 +90,29 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.core-button > a {
-    @apply font-serif outline-none;
-    background-color: #c386ae;
-}
+.core-button {
+    @apply relative;
 
-.core-button.--uppercase > a {
-    @apply uppercase;
+    & > a {
+        @apply font-sans outline-none;
+    }
 }
 
 .core-button:not(.--is-link),
 .core-button.--is-link > a {
     @apply inline-flex items-center justify-center outline-none;
     line-height: 1.25rem;
-    border-radius: 5px;
 }
 
-.core-button.--large:not(.--is-link),
-.core-button.--large.--is-link > a {
-    @apply font-bold;
-    height: 87px;
-    font-size: 28px;
-    padding: 12px 40px;
-    border-width: 3px;
-    min-width: 128px;
+.core-button:not(.--is-link) {
+    @apply cursor-default;
+    background-color: #a8b4ca;
+    border-color: #a8b4ca;
+    color: #cdd2e2;
 }
 
-.core-button.--medium:not(.--is-link),
-.core-button.--medium.--is-link > a {
-    @apply h-12 px-7 py-3.5 font-bold text-base border-4;
-    min-width: 108px;
-}
-
-.core-button.--small:not(.--is-link),
-.core-button.--small.--is-link > a {
-    @apply h-10 px-5 py-3 text-xs;
-    min-width: 80px;
-    border-width: 3px;
+.core-button.--uppercase > a {
+    @apply uppercase;
 }
 
 .core-button.--rounded:not(.--is-link),
@@ -132,23 +120,123 @@ export default {
     border-radius: 40px;
 }
 
-.core-button.--primary:not(.--is-link),
-.core-button.--primary.--is-link > a {
-    color: #1a1a30;
-    border-color: currentColor;
+.core-button.--primary {
+    & > a {
+        @apply z-10 border-0;
+        color: #f0ebf5;
+        background-image: linear-gradient(
+            276.15deg,
+            #72dcb6 0.25%,
+            #3849de 52.99%,
+            #9b4dc3 93.14%
+        );
+
+        & span {
+            @apply relative z-30;
+        }
+
+        &:hover:after {
+            @apply opacity-100;
+        }
+    }
+
+    &.--large:not(.--is-link),
+    &.--large > a {
+        @apply font-bold;
+        height: 87px;
+        font-size: 28px;
+        padding: 24px 48px;
+        min-width: 128px;
+    }
+
+    &.--medium:not(.--is-link),
+    &.--medium > a {
+        @apply font-semibold;
+        height: 68px;
+        font-size: 24px;
+        padding: 16px 48px;
+        min-width: 108px;
+    }
+
+    &.--small:not(.--is-link),
+    &.--small > a {
+        @apply h-12 font-semibold;
+        padding: 12px 36px;
+        min-width: 80px;
+    }
+}
+.core-button.--primary > a:after {
+    @apply absolute top-0 left-0 h-full w-full;
+    @apply opacity-0 z-20;
+    border-radius: inherit;
+    content: '';
+    background-image: linear-gradient(
+        96.26deg,
+        #74dcb6 5.5%,
+        #4454df 50.05%,
+        #9d51c3 82.35%
+    );
+    transition: opacity 0.5s ease-out;
 }
 
-.core-button.--secondary:not(.--is-link),
-.core-button.--secondary.--is-link > a {
-    color: #1a1a30;
-    border-color: currentColor;
-    font-family: unset;
-}
+.core-button.--secondary {
+    & > a {
+        @apply z-10;
+        background-image: none;
+        background-color: #e099e1;
 
-.core-button:not(.--is-link):hover,
-.core-button.--is-link > a:hover {
-    color: #c386ae;
-    background-color: #121023;
+        & span {
+            @apply relative z-30;
+            color: #1a1a30;
+        }
+
+        &:hover span {
+            color: #e099e1;
+        }
+
+        &:hover:after {
+            @apply opacity-100;
+        }
+    }
+
+    &.--large:not(.--is-link),
+    &.--large > a {
+        @apply font-bold;
+        height: 57px;
+        font-size: 18px;
+        padding: 16px 36px;
+    }
+
+    &.--medium:not(.--is-link),
+    &.--medium > a,
+    &.--small:not(.--is-link),
+    &.--small > a {
+        @apply font-bold;
+        height: 42px;
+        font-size: 16px;
+        padding: 10px 22px;
+    }
+}
+.core-button.--secondary {
+    & > a:after {
+        @apply absolute top-0 left-0 w-full h-full;
+        @apply opacity-0 z-20;
+        content: '';
+        background-image: none;
+        background-color: #1a1a30;
+        border-radius: inherit;
+        border-color: #e099e1;
+        transition: opacity 0.5s ease-out;
+    }
+
+    &.--small > a:after,
+    &.--medium > a:after {
+        border-width: 3px;
+    }
+
+    &.--large > a:after {
+        border-width: 2px;
+    }
 }
 
 .core-button.--medium.--bulletin:not(.--is-link),
