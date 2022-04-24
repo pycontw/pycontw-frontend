9f8d259176555d18899a208f8a712aabfd1903f4
TC Wang <tcwang@M1165-TC-Wang-de-MacBook-Pro.local>
2022-04-24 22:29:33 +0800

    feat: add bulletin card samples

--- components/bulletins/BulletinCard.vue
+++ components/bulletins/BulletinCard.vue
@@ -27,12 +27,12 @@
 </template>
 
 <script>
-import TextButton from '@/components/core/buttons/TextButton'
+// import TextButton from '@/components/core/buttons/TextButton'
 
 export default {
     name: 'BulletinCard',
     components: {
-        TextButton,
+        // TextButton,
     },
     props: {
         title: { type: String, default: '' },
@@ -40,9 +40,9 @@ export default {
             type: String,
             default: '',
         },
-        linkTitle: { type: String, default: '' },
-        linkHref: { type: String, default: '' },
-        linkTo: { type: String, default: '' },
+        // linkTitle: { type: String, default: '' },
+        // linkHref: { type: String, default: '' },
+        // linkTo: { type: String, default: '' },
     },
     computed: {
         classObject() {
@@ -56,24 +56,28 @@ export default {
 
 <style lang="postcss" scoped>
 .bulletinCard {
-    @apply relative flex flex-col items-center font-serif rounded-3xl border-4;
-    width: 47%;
-    @media (min-width: 415px) {
-        width: 214px;
-    }
-    border-color: #4b4b4b;
-    color: #4b4b4b;
+    @apply relative flex flex-col justify-center items-center rounded-xl;
+    padding: 25px 3px;
+    width: 236px;
+    height: 310px;
+    left: 284px;
+    top: 0px;
+    margin: 48px;
+    color: #a9a6d9;
+    background: #1f1c3b;
 }
 
 .bulletinCard:hover {
-    border-color: #413f5a;
+    border-color: #746bb8;
+    background: #352d66;
+    color: #f0ebf5;
 }
 
 /* A trick to set height to be equal to width. */
 /* Checkout: https://stackoverflow.com/a/6615994/7969188 */
 .bulletinCard > .dummy {
     margin-top: 230%;
-    @media (min-width: 370px) {
+    @media (min-width: 375px) {
         margin-top: 170%;
     }
 }
@@ -84,29 +88,25 @@ export default {
 
 .bulletinCard__content > h2 {
     @apply text-base text-center mt-6 mb-5 font-bold;
-    @media (min-width: 415px) {
+    font-family: 'Noto Serif TC';
+    @media (min-width: 375px) {
         @apply text-xl mt-8 mb-5;
     }
-    color: #808080;
-}
-
-.bulletinCard__content:hover > h2 {
-    color: #7568f6;
 }
 
 .bulletinCard__content > p {
     @apply mx-2;
     font-size: 0.6rem;
-    color: #c7c7c7;
-    @media (min-width: 415px) {
+    font-family: 'Source Sans Pro';
+    @media (min-width: 375px) {
         font-size: 0.8rem;
     }
 }
 
-.bulletinCard__button {
+/* .bulletinCard__button {
     @apply absolute bottom-5 cursor-pointer;
     @media (min-width: 415px) {
         @apply bottom-7;
     }
-}
+} */
 </style>
