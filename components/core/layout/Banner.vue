3992e5e16b59cf83350f294a6aaf86ef4a698086
Sivan <sivanyeh@gmail.com>
2022-06-04 13:38:06 +0800

    fix:switch styles with page size

--- components/core/layout/Banner.vue
+++ components/core/layout/Banner.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="update1 h-96 font-serif font-semibold w-full flex-1">
-        <div class="image md:block w-full md:w-6/6 md:h-64">
+    <div class="h-96 font-serif font-semibold w-full flex-1">
+        <div class="maxpage invisible md:visible md: w-full md:h-64">
+            <slot name="standard"></slot>
+        </div>
+        <div class="minpage w-6/6 h-64 md:invisible md:w-full md:h-0">
             <slot name="standard"></slot>
         </div>
     </div>
@@ -9,17 +12,11 @@
 <script>
 export default {
     name: 'Banner',
-    props: {
-        backgroundImagePath: {
-            type: String,
-            default: ' ~/static/img/about/WebBanner.svg',
-        },
-    },
 }
 </script>
 
 <style lang="postcss" scoped>
-.update1 {
+.maxpage {
     background-image: url('~/static/img/about/WebBanner.svg');
     background-repeat: no-repeat;
     background-position: 75% 75%;
@@ -29,14 +26,15 @@ export default {
     flex-direction: column;
     text-align: center;
 }
-.update2 {
+.minpage {
     background-image: url('~/static/img/about/RWDBanner.svg');
     background-repeat: no-repeat;
-    background-position: center;
-    background-size: contain;
+    background-position: 0% 65%;
+    background-size: 150%;
     display: flex;
     justify-content: center;
     flex-direction: column;
     text-align: center;
+    word-wrap: break-word;
 }
 </style>
