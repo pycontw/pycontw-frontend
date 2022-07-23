e7805d5d729fca9d51dac5de684b0b6db0a21a2a
Tim Tsai <timtsai@91app.com>
2022-07-23 21:09:49 +0800

    feat(tickets): add sponosr pass content
    fix(tickets): fixz sponsorcontent typo

--- pages/registration/tickets.vue
+++ pages/registration/tickets.vue
@@ -82,15 +82,13 @@
                     </div>
                 </div>
             </div>
-            <!-- TODO
-                等文案 Review 後再放
+            <!-- Sponsor pass content-->
             <div class="pb-12">
                 <h2 id="introSponsor">{{ $t('introSponsor') }}</h2>
                 <i18n path="SponsorContent" tag="p" class="SponsorContent">
                     <template #br><br /></template>
                 </i18n>
             </div>
-            -->
         </i18n-page-wrapper>
     </div>
 </template>
