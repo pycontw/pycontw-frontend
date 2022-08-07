9536b53ed5cac4853cc4e78fdaac1edef63ce6f1
tim tsai <tim08094495757@gmail.com>
2022-08-08 02:28:06 +0800

    fix(keynotes): remove pre-line

--- pages/conference/keynotes.vue
+++ pages/conference/keynotes.vue
@@ -62,12 +62,12 @@
                 </h2>
                 <tabs class="keynote__tabs">
                     <tab :title="$t('terms.bio')">
-                        <div class="pb-4 whitespace-pre-line">
+                        <div class="pb-4">
                             {{ keynote.speaker[$makeKey(locale, 'bio')] }}
                         </div>
                     </tab>
                     <tab :title="$t('terms.talk')">
-                        <div class="pb-4 whitespace-pre-line">
+                        <div class="pb-4">
                             {{
                                 keynote.session[$makeKey(locale, 'description')]
                             }}
