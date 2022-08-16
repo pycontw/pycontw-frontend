ad4e080853c4481202d563197267facebe668677
yangche.su <yangche.su@rakuten.com>
2022-08-16 11:31:41 +0900

    feat(talks, tutorials): Update the content of talks and tutorials, and fix keynotes info

--- i18n/conference/keynotes.i18n.js
+++ i18n/conference/keynotes.i18n.js
@@ -4,7 +4,7 @@ export default genI18nMessages({
     'en-us': {
         title: 'Keynote Speech',
         intro:
-            'PyCon APAC 2022 invites four speakers to give keynote speeches during the ' +
+            'PyCon APAC 2022 invites five speakers to give keynote speeches during the ' +
             'two-day conference. ' +
             'Each keynote speaker is considered one of the most important figures ' +
             'in their respective fields. ' +
@@ -17,7 +17,7 @@ export default genI18nMessages({
     'zh-hant': {
         title: '主題演講',
         intro:
-            '為期兩天的議程中，將有四位講者於不同場次進行主題演講（Keynote）。{br}' +
+            '為期兩天的議程中，將有五位講者於不同場次進行主題演講（Keynote）。{br}' +
             '主題演講講者都是在全球而言各領域非常頂尖的人士；{br}' +
             '他們將帶來對自己的領域、專案的經驗分享，或者對於未來數年發展的方向與想像。',
         terms: {
