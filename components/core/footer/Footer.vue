5813d8fc19a36713cdd2ef5baec2b5553e03e27a
Sivan <sivanyeh@gmail.com>
2022-08-17 19:06:58 +0800

    fix: nav-bar to footer

--- components/core/footer/Footer.vue
+++ components/core/footer/Footer.vue
@@ -14,9 +14,9 @@
                     <locale-link to="/about/staff" class="my-2">{{
                         $t('staff')
                     }}</locale-link>
-                    <!-- <locale-link to="/about/privacy-policy" class="my-2">
+                    <locale-link to="/about/privacy-policy" class="my-2">
                         {{ $t('privacyPolicy') }}
-                    </locale-link> -->
+                    </locale-link>
                 </div>
             </div>
             <footer-history />
