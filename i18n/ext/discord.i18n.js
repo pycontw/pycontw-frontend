63f3a9ab4e380ab6136e66dfa9647259f8b32869
Tim Tsai <timtsai@91app.com>
2022-08-25 04:15:47 +0800

    feat(ext/discord): add 2022 discord guideline

--- i18n/ext/discord.i18n.js
+++ i18n/ext/discord.i18n.js
@@ -4,17 +4,14 @@ export default genI18nMessages({
     'en-us': {
         invalidAttendee:
             'The token within the link is invalid. Please contact staff for further help.',
-        title: 'PyCon TW Discord Guideline',
-        pageAbstract:
-            'If you have not installed Discord or joined Python Taiwan Discord server, ' +
-            'follow {setup} and setup your Discord first.',
-        setupText: 'Setting up Discord',
-
         tutorialsHeader: 'Tutorials',
         registration: {
             title: '1. Register yourself',
             descriptions: [
-                'Users can find a {registrationDesk} channel under {info} category. ' +
+                'If you have not installed Discord or joined Python Taiwan Discord server, ' +
+                    'follow {setup} and setup your Discord first.{br}' +
+                    '{br}' +
+                    'Users can find a {registrationDesk} channel under {info} category. ' +
                     'There is a PyCon TW RegBot in this channel which guides you to ' +
                     'insert the command {registrationCommand} for registration and assigns you a role.{br}' +
                     'In order to avoid token leakage, you CANNOT send message to {registrationDesk} channel.',
@@ -23,6 +20,7 @@ export default genI18nMessages({
                     'in member list on the right side and directly message one of them, ' +
                     'or please send a message to {general} channel and mention {mentionManager}.',
             ],
+            setupText: 'Setting up Discord',
         },
         announcement: {
             title: '2. Take a look at official announcements',
@@ -47,36 +45,23 @@ export default genI18nMessages({
             'enter some private channel.',
         roleList: [
             {
-                name: 'discord manager',
-                description: 'Target: Discoard managers',
+                name: '2022-attendee',
+                description: 'Target: PyCon APAC 2022 attendees',
             },
             {
-                name: '2021-session-chair',
-                description:
-                    'Target: PyCon TW 2021 session chairs and assistant chairs',
+                name: '2022-staff',
+                description: 'Target: PyCon APAC 2022 staff',
             },
             {
-                name: '2021-staff',
-                description: 'Target: PyCon TW 2021 staff',
-            },
-            {
-                name: '2021-speaker',
-                description: 'Target: PyCon TW 2021 speakers',
-            },
-            {
-                name: '2021-attendee',
-                description: 'Target: PyCon TW 2021 attendees',
-            },
-            {
-                name: '2021-sponsor',
-                description: 'Target: PyCon TW 2021 sponsors',
+                name: '2022-speaker',
+                description: 'Target: PyCon APAC 2022 speakers',
             },
         ],
-        channelListHeader: 'PyCon TW 2021 Channels list',
+        channelListHeader: 'PyCon APAC 2022 Channels list',
         role: 'Roles:',
         channelGroups: [
             {
-                type: 'Announcement/Help Desk (INFORMATION Category)',
+                type: 'Announcement/Help Desk',
                 channels: [
                     {
                         tag: 'registration-desk',
@@ -92,97 +77,44 @@ export default genI18nMessages({
                             'The official announcement channel, any official announcements will be posted here.',
                     },
                     {
-                        tag: 'staff-announcements',
-                        description:
-                            'An announcement channel set up specifically for staff. ' +
-                            'Staff are also encouraged to communicate with each other in this channel.',
-                    },
-                    {
                         tag: 'information-desk',
                         description:
                             'The official help desk, you may ask any open question you have in mind.',
                     },
                     {
-                        tag: 'gather-front-desk',
-                        description:
-                            'The official help desk for gather.town, you may ask any question related to gather.town here.',
-                    },
-                    {
                         tag: 'lightning-talk',
                         description:
                             'The information about lightning talk will be announced here. ' +
-                            'Nominatee should make a checkin in this channel before their talk.{br}' +
-                            'Application deadline: 10/2, 10/3 13:00',
+                            'Nominatee should make a checkin in this channel before their talk.',
                     },
                     {
-                        tag: 'speakers',
+                        tag: 'open-space',
                         description:
-                            'Speakers only information desk, if there’s any session-related or speaker-related questions, ' +
-                            'it’s encouraged to raised them here for discussion.',
+                            'The information about Open Space will be announced here.',
                     },
                     {
-                        tag: 'session-chairs',
+                        tag: 'booth-game',
                         description:
-                            'A channel for session chairs and assistant chairs to give any related questions here ' +
-                            'for answers or further instruction.',
+                            'The information about Booth Game will be announced here.',
                     },
                 ],
             },
             {
-                type: 'Session channel (TRACKS Category)',
+                type: 'Session channel',
                 channels: [
                     {
-                        tag: 'keynote-track',
+                        tag: 'program-keynote',
                         description: 'A discussion channel for Keynotes.',
                     },
                     {
-                        tag: 'r0-track',
-                        description:
-                            'A discussion channel for all sessions in R0 track.',
+                        tag: 'program-talk',
+                        description: 'A discussion channel for all sessions.',
                     },
                     {
-                        tag: 'r1-track',
+                        tag: 'program-tutorial',
                         description:
-                            'A discussion channel for all sessions in R1 track.',
+                            'A discussion channel for tutorial sessions.',
                     },
-                    {
-                        tag: 'r2-track',
-                        description:
-                            'A discussion channel for all sessions in R2 track.',
-                    },
-                    {
-                        tag: 'r3-track',
-                        description:
-                            'A discussion channel for all sessions in R3 track.',
-                    },
-                ],
-            },
-            {
-                type: 'Sponsor channel (SPONSOR Category)',
-                channels: [
-                    {
-                        tag: 'cathay',
-                        description:
-                            'With 1x text channel and 1x audio channel.',
-                    },
-                    {
-                        tag: 'kloudless',
-                        description:
-                            'With 1x text channel and 1x audio channel.',
-                    },
-                    { tag: 'esun', description: 'With 1x text channel.' },
-                    { tag: 'berry-ai', description: 'With 1x text channel.' },
-                    { tag: 'sinopac', description: 'With 1x text channel.' },
-                    { tag: 'appier', description: 'With 1x text channel.' },
-                    { tag: 'ichef', description: 'With 1x text channel.' },
-                    {
-                        tag: 'kkbox-group',
-                        description: 'With 1x text channel.',
-                    },
-                    { tag: 'onedegree', description: 'With 1x text channel.' },
-                    { tag: 'ctbc', description: 'With 1x text channel.' },
-                    { tag: 'tagtoo', description: 'With 1x text channel.' },
-                    { tag: 'tenlong', description: 'With 1x text channel.' },
                 ],
             },
         ],
@@ -219,16 +151,14 @@ export default genI18nMessages({
     'zh-hant': {
         invalidAttendee:
             '連結中附帶的驗證碼錯誤。請聯繫工作人員取得更進一步的協助。',
-        title: 'PyCon TW Discord 指南',
-        pageAbstract:
-            '如果尚未安裝 Discord 並加入 Python Taiwan 的伺服器，請先閱讀{setup}。',
-        setupText: '設定 Discord 與加入 Python Taiwan 伺服器',
+        tutorialsHeader: 'PyCon APAC Discord 指南',
 
-        tutorialsHeader: '使用流程',
         registration: {
             title: '1. 註冊',
             descriptions: [
-                '剛加入伺服器後，使用者會在 {info} 的類別下看到一個 {registrationDesk} 頻道。' +
+                '如果尚未安裝 Discord 並加入 Python Taiwan 的伺服器，請先閱讀 {setup} 。{br}' +
+                    '{br}' +
+                    '剛加入伺服器後，使用者會在 {info} 的類別下看到一個 {registrationDesk} 頻道。' +
                     '此頻道會提供註冊教學，教導使用者進入與 PyCon TW Reg Bot 的一對一頻道' +
                     '中輸入 {registrationCommand} 以進行註冊，並由機器人給予使用者對應的身份組。{br}' +
                     '為避免使用者不慎將 token 外流，您無法直接傳送訊息至 {registrationDesk} 頻道',
@@ -236,6 +166,7 @@ export default genI18nMessages({
                     '如果持續無法成功註冊，請點選右方成員清單中身份組為 {manager} 的成員並直接傳訊息給他們，' +
                     '或是將您的問題在 {general} 頻道中提出並 mention {mentionManager}。',
             ],
+            setupText: '設定 Discord 與加入 Python Taiwan 伺服器',
         },
         announcement: {
             title: '2. 查看大會公告',
@@ -246,7 +177,7 @@ export default genI18nMessages({
         joinChannels: {
             title: '3. 加入議程討論頻道',
             descriptions: [
-                '議程討論頻道皆位在 {tracks} 類別下。共有五個頻道：',
+                '議程討論頻道皆位在 {tracks} 類別下。共有三個頻道：',
                 '{channels}',
                 '請根據keynote時段或是您想參與的議程軌進入對應的討論頻道。{br}' +
                     '若該時段為keynote時段，請您前往 {keynote} 頻道參與討論',
@@ -255,38 +186,26 @@ export default genI18nMessages({
 
         roleListHeader: '身份組清單',
         roleListDescription:
-            '以下是 PyCon TW 2021 相關的 Discord 身份組。您可以點選使用者的大頭貼以確認該位使用者的身份。',
+            '以下是 PyCon APAC 2022 相關的 Discord 身份組。您可以點選使用者的大頭貼以確認該位使用者的身份。',
         roleList: [
             {
-                name: 'discord manager',
-                description: '對象：Discord 管理員',
+                name: '2022-attendee',
+                description: '對象：PyCon APAC 2022 的會眾',
             },
             {
-                name: '2021-session-chair',
-                description: '對象：PyCon TW 2021 的主持人',
+                name: '2022-speaker',
+                description: '對象：PyCon APAC 2022 的講者',
             },
             {
-                name: '2021-staff',
-                description: '對象：PyCon TW 2021 的志工',
-            },
-            {
-                name: '2021-speaker',
-                description: '對象：PyCon TW 2021 的講者',
-            },
-            {
-                name: '2021-attendee',
-                description: '對象：PyCon TW 2021 的會眾',
-            },
-            {
-                name: '2021-sponsor',
-                description: '對象：PyCon TW 2021 的贊助商',
+                name: '2022-staff',
+                description: '對象：PyCon APAC 2022 的志工',
             },
         ],
         channelListHeader: '頻道清單',
         role: '身份組：',
         channelGroups: [
             {
-                type: '大會公告 / 服務台 相關 ( INFORMATION 類別)',
+                type: 'Announcement/Help Desk',
                 channels: [
                     {
                         tag: 'registration-desk',
@@ -300,84 +219,51 @@ export default genI18nMessages({
                             '大會公告頻道，若大會有全面性的公告事項都會發在此處。',
                     },
                     {
-                        tag: 'staff-announcements',
+                        tag: 'announcements',
                         description:
-                            '一個大會專門公告給志工的頻道，志工們亦可在此頻道互相聯繫。',
+                            '大會公告頻道，若大會有全面性的公告事項都會發在此處。',
                     },
                     {
                         tag: 'information-desk',
                         description:
-                            '大會的服務台，有任何公開詢問的問題可以在此處提出。',
-                    },
-                    {
-                        tag: 'gather-front-desk',
-                        description:
-                            '大會 gather.town 的服務台，任何有關 gather.town 的問題可以在此處提出。',
+                            '大會的服務台，有任何想要詢問的問題皆可以在此處提出。',
                     },
                     {
                         tag: 'lightning-talk',
                         description:
-                            '閃電秀的資訊都會公告在此頻道。lightning talk 講者屆時需在這個頻道進行報到。{br}' +
-                            '報名截止：10/2,3 13:00',
+                            '閃電秀的資訊都會公告在此頻道。lightning talk 講者屆時需在這個頻道進行報到。',
                     },
                     {
-                        tag: 'speakers',
+                        tag: 'open-space',
                         description:
-                            '講者專屬的服務台，有議程或講者相關的問題可以在此處提出與討論。',
+                            'Open Space 資訊交換頻道。您可以分享連結、影音等資訊。',
                     },
                     {
-                        tag: 'session-chairs',
+                        tag: 'booth-game',
                         description:
-                            '主持人們的討論頻道，有任何主持人相關的問題可以在此處提出與討論。',
+                            '大地遊戲相關資訊頻道。大地遊戲相關資訊與任務通知都會在此公告，{br}' +
+                            '歡迎大家一起到 Gather.Town 線上會場跟著貓將軍遶境解任務！',
                     },
                 ],
             },
             {
-                type: '議程 相關 ( TRACKS 類別)',
+                type: 'Session channel',
                 channels: [
                     {
-                        tag: 'keynote-track',
-                        description: 'Keynote 的討論頻道。',
+                        tag: 'program-keynote',
+                        description:
+                            'Keynote 相關資訊頻道（共筆、slido、YouTube 直播連結），與 Keynote 議程相關的討論也會在此進行。',
                     },
                     {
-                        tag: 'r0-track',
-                        description: 'R0 軌所有議程的會眾討論頻道。',
+                        tag: 'program-talk',
+                        description:
+                            'Talk 相關資訊頻道（共筆、slido、Gather 連結），與 Talk 議程相關的討論也會在此進行。',
                     },
                     {
-                        tag: 'r1-track',
-                        description: 'R1 軌所有議程的會眾討論頻道。',
+                        tag: 'program-tutorial',
+                        description:
+                            'Tutorial 相關資訊頻道（共筆、slido、Gather 連結），與 Tutorial 議程相關的討論也會在此進行。',
                     },
-                    {
-                        tag: 'r2-track',
-                        description: 'R2 軌所有議程的會眾討論頻道。',
-                    },
-                    {
-                        tag: 'r3-track',
-                        description: 'R3 軌所有議程的會眾討論頻道。',
-                    },
-                ],
-            },
-            {
-                type: '贊助商 相關 ( SPONSORS 類別)',
-                channels: [
-                    {
-                        tag: 'cathay',
-                        description: '共有一個文字頻道與一個語音頻道',
-                    },
-                    {
-                        tag: 'kloudless',
-                        description: '共有一個文字頻道與一個語音頻道',
-                    },
-                    { tag: 'esun', description: '共有一個文字頻道' },
-                    { tag: 'berry-ai', description: '共有一個文字頻道' },
-                    { tag: 'sinopac', description: '共有一個文字頻道' },
-                    { tag: 'appier', description: '共有一個文字頻道' },
-                    { tag: 'ichef', description: '共有一個文字頻道' },
-                    { tag: 'kkbox-group', description: '共有一個文字頻道' },
-                    { tag: 'onedegree', description: '共有一個文字頻道' },
-                    { tag: 'ctbc', description: '共有一個文字頻道' },
-                    { tag: 'tagtoo', description: '共有一個文字頻道' },
-                    { tag: 'tenlong', description: '共有一個文字頻道' },
                 ],
             },
         ],
@@ -391,7 +277,7 @@ export default genI18nMessages({
             },
             {
                 content:
-                    '如果您沒有Discord帳戶，請註冊一個帳戶。' +
+                    '如果您沒有 Discord 帳戶，請註冊一個帳戶。' +
                     "打開剛剛安裝好的 Discord 程式，按下 '註冊'('Register') 並完成你的註冊流程。" +
                     '(Discord 會寄一封驗證信到你的信箱，請到信箱收信並完成驗證){br}{img0}',
             },
