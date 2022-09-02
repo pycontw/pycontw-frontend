04d62d2263e9417bfb4e34d38c80e24d7c6e1645
Sivan <sivanyeh@gmail.com>
2022-09-02 22:44:17 +0800

    Update apac-community.i18n.js

--- i18n/about/apac-community.i18n.js
+++ i18n/about/apac-community.i18n.js
@@ -12,12 +12,45 @@ export default genI18nMessages({
             'Learn something new, teach something new, find opportunities, build cool things, make friends, geek out! Come celebrate and enjoy this event with us! ',
         communities: [
             {
+                tag: 'Japan',
+                title: 'PyCon Japan',
+                description:
+                    'PyCon JP is a conference for Python users to gather, exchange information and interact with Python and software using Python. ' +
+                    'Through PyCon JP, the goal is to gather Python users together, exchange knowledge and information on various fields related to Python, connect with new friends and communities, and increase work and business opportunities.',
+            },
+            {
+                tag: 'Korea',
+                title: 'PyCon Korea',
+                description:
+                    'PyCon Korea is a non-profitable, developer-oriented event organized by community members and has different characteristics from conferences managed companies or organizations. ' +
+                    'All attendees, including speakers, tutorial hosts, voluntary workers, ' +
+                    'and organizers, participate in PyCon Korea without any profitable purpose and All members are volunteers. ' +
+                    'We organized to broaden the usage of Python, an open-source programming language, and activate its community.',
+            },
+            {
+                tag: 'HongKong',
+                title: 'PyCon Hong Kong',
+                description:
+                    'PyCon HK physical conference is back! ' +
+                    'Our theme this year is “Delight Yourself in Python”, you are cordially invited to submit your proposed topics! ' +
+                    'Don’t miss this chance to speak to our python community in person!',
+            },
+            {
+                tag: 'Philippines',
+                title: 'PyCon Philippines',
+                description:
+                    'Python PH is a non-stock, non-profit, volunteer-run organization dedicated to support and facilitate the growth of the community of Python programmers in the Philippines. ' +
+                    'We believe that we create small dents of improvement in the PH Tech industry when we foster environments that inspire and motivate individuals to be technically competent and passionate about their craft.',
+            },
+            {
                 tag: 'NewZealand',
                 title: 'Kiwi PyCon',
                 description:
-                    'New Zealand Python User Group (NZPUG) aims to support and promote the use of Python in New Zealand. ' +
-                    'Our members volunteer their time, expertise, and effort to promote and educate about Python all over the country. ' +
-                    'Join us and help to make Python the most accessible, best supported, most fun, and most used programming language in New Zealand.',
+                    'Kiwi PyCon is the Python community gathered to meet and to mingle, to share experiences, to learn from each other, and to build lasting relationships. ' +
+                    'When we congregate we become Kiwi PyCon. ' +
+                    'Kiwi PyCon moves around the country to give all community members the chance to attend the conference near their place of residence. ' +
+                    'This year Kiwi PyCon is going hybrid! ' +
+                    'It will happen simultaneously in-person, from our central hub in The Arts Centre in Ōtautahi Christchurch, and online, using Venueless as our online platform.',
             },
             {
                 tag: 'Australia',
@@ -29,14 +62,28 @@ export default genI18nMessages({
                     'as well as the libraries and frameworks that they rely on.',
             },
             {
-                tag: 'Japan',
-                title: 'PyCon Japan',
+                tag: 'Indonesia',
+                title: 'PyCon Indonesia',
                 description:
-                    'PyCon Japan 2022 is Japan’s largest Python conference for Python users to gather and exchange information about Python and software using Python. ' +
-                    'In 2020 and 2021, it was held online and hybrid respectively, ' +
-                    'but in 2022, we will once again gather offline to exchange knowledge and information in various fields related to Python and make new friends. ' +
-                    'It is our goal to create a place where we can connect with the community, work and business opportunities. ' +
-                    'We are still finalizing our logo for PyCon 2022, so please give us some more time.',
+                    'Python is everywhere. It’s in your favorite apps, it’s used in companies worldwide, it even runs governments. ' +
+                    'Python has massive impact, not only in computing world, but the entire world. ' +
+                    'PyCon Indonesia returns with renewed spirit to promote Python Everywhere, this is our theme for PyCon Indonesia. ' +
+                    'We invite everyone from all walks of life to share their experience with Python. Join us to promote Python Everywhere.',
+            },
+            {
+                tag: 'Singapore',
+                title: 'PyCon Singapore',
+                description:
+                    'PyCon Singapore is a conference about the programming language Python. ' +
+                    'It is a community effort driven by the members of the Python User Group Singapore.',
+            },
+            {
+                tag: 'Malaysia',
+                title: 'PyCon Malaysia',
+                description:
+                    'PyCon MY 2022 will be different compared to last few PyCons. ' +
+                    'Apart from enjoying the scenery at Kota Kinabalu, Sabah, we will be having a small gathering with different sprints projects for attendees to work with. ' +
+                    'This is also an exclusive event since we will only be admitting 80 attendees for this PyCon.',
             },
             {
                 tag: 'Thailand',
@@ -49,13 +96,12 @@ export default genI18nMessages({
                     'PyCon Thailand also provides financial support to non-professional Python community of Thailand.',
             },
             {
-                tag: 'Malaysia',
-                title: 'PyCon Malaysia',
+                tag: 'India',
+                title: 'PyCon India',
                 description:
-                    'PyCon Malaysia organising community is always small and closed knitted one. ' +
-                    'Most of the committee members have been involving in multiple PyCon MY. Some seniors like Iqbal, Swee Meng, ' +
-                    'Kei Yan and the members of Kinabalu Coders have been helping out multiple events. ' +
-                    'For 2022, the main committee member are made of of Hilmi, James and Ivy.',
+                    'PyCon India is the largest annual gathering of Python aficionados and experts in India where Pythonistas come from around the world to celebrate their use of Python, ' +
+                    'and share their knowledge and experience. ' +
+                    'It is a volunteer-driven conference aimed to foster the adoption, use, and development of the Python programming language.',
             },
             {
                 tag: 'Russia',
@@ -66,55 +112,6 @@ export default genI18nMessages({
                     'St. Petersburg, Yekaterinburg, Kazan, Nizhny Novgorod, Samara, Perm and other cities. PyCon Russia has a special format: ' +
                     'participants and speakers spend two days outdoors, listen to reports and master classes from Russian and foreign speakers, ' +
                     'perform at Lightning Talks, and in the evening have fun at an afterparty with a fire and songs under guitar.',
-            },
-            {
-                tag: 'HongKong',
-                title: 'PyCon Hong Kong',
-                description:
-                    'During the visit of PyCon APAC 2013 in Tokyo, ' +
-                    'Sammy met with other PyCon APAC organisers and found that PyCon have been held in many cities in Asia except Hong Kong. ' +
-                    'Upon return, Sammy and Open Source Hong Kong organised the first PyCon Hong Kong in 2015. ' +
-                    'This event becomes one of the catalysts in promoting and driving the proliferation of Python development and usage in the city.',
-            },
-            {
-                tag: 'Singapore',
-                title: 'PyCon Singapore',
-                description:
-                    'PyCon Singapore is a conference about the programming language Python. ' +
-                    'It is a community effort driven by the members of the Python User Group Singapore.',
-            },
-            {
-                tag: 'Indonesia',
-                title: 'PyCon Indonesia',
-                description:
-                    'Python is everywhere. It’s in your favorite apps, it’s used in companies worldwide, it even runs governments. ' +
-                    'Python has massive impact, not only in computing world, but the entire world. ' +
-                    'PyCon Indonesia returns with renewed spirit to promote Python Everywhere, this is our theme for PyCon Indonesia. ' +
-                    'We invite everyone from all walks of life to share their experience with Python. Join us to promote Python Everywhere.',
-            },
-            {
-                tag: 'Korea',
-                title: 'PyCon Korea',
-                description:
-                    'PyCon Korea is a non-profitable, developer-oriented event organized by community members and has different characteristics from conferences managed companies or organizations. ' +
-                    'All attendees, including speakers, tutorial hosts, voluntary workers, ' +
-                    'and organizers, participate in PyCon Korea without any profitable purpose and All members are volunteers. ' +
-                    'We organized to broaden the usage of Python, an open-source programming language, and activate its community.',
-            },
-            {
-                tag: 'India',
-                title: 'PyCon India',
-                description:
-                    'PyCon India is the largest annual gathering of Python aficionados and experts in India where Pythonistas come from around the world to celebrate their use of Python, ' +
-                    'and share their knowledge and experience. ' +
-                    'It is a volunteer-driven conference aimed to foster the adoption, use, and development of the Python programming language.',
-            },
-            {
-                tag: 'Philippines',
-                title: 'PyCon Philippines',
-                description:
-                    'PyCon Philippines is a volunteer-run, not-for-profit conference centered around the Python Programming language. ' +
-                    'The goal of the conference is to provide a venue where the Python programming language and surrounding technologies can be explored, discussed and exercised.',
             },
         ],
         website: 'Website',
@@ -131,12 +128,44 @@ export default genI18nMessages({
             '學習新東西，教新東西，尋找機會，打造酷炫的東西，結交朋友，搞怪！快來和我們一起慶祝和享受這個活動吧！',
         communities: [
             {
+                tag: 'Japan',
+                title: 'PyCon Japan',
+                description:
+                    'PyCon Japan 是聚集 Python 使用者、交流使用 Python 與 Python 軟體進行的研討會議。' +
+                    '透過 PyCon Japan，我們的目標是創造一個可以與社群、工作和商業機會聯繫的地方。',
+            },
+            {
+                tag: 'Korea',
+                title: 'PyCon Korea',
+                description:
+                    'PyCon Korea 是由社群成員組織的非營利性、面向開發者的研討會，具有與會議管理的公司或組織不同的特點。' +
+                    '所有參與 PyCon Korea 的與會者，包括演講者、教學主持人、志工和組織者皆以非盈利目的，' +
+                    '所有成員都是志工。我們組織起來以擴大 Python 的使用範圍並活絡社群。',
+            },
+            {
+                tag: 'HongKong',
+                title: 'PyCon Hong Kong',
+                description:
+                    'PyCon HK 實體會議又回來了！' +
+                    '今年我們的主題是 “Delight Yourself in Python”，誠摯地邀請您來投稿！' +
+                    '不要錯過這個與我們的 Python 社群面對面交流的機會！',
+            },
+            {
+                tag: 'Philippines',
+                title: 'PyCon Philippines',
+                description:
+                    'Python Philippines 是一個非營利性的志工組織，致力於支持和促進菲律賓 Python 工程師社群的發展。' +
+                    '我們相信，當我們營造出能夠激發個人在技術上勝任，並對技術充滿熱情的環境時，我們會在菲律賓技術產業中取得小小的進步。',
+            },
+            {
                 tag: 'NewZealand',
                 title: 'Kiwi PyCon',
                 description:
-                    '紐西蘭 Python 使用者群組 (NZPUG) 旨在支持和促進 Python 在紐西蘭的使用。' +
-                    '我們的成員自願貢獻他們的時間、專業知識和精力，在全國推廣和教育 Python。' +
-                    '加入我們並幫助使 Python 成為紐西蘭最容易訪問、最受支持、最有趣和最常用的程式語言。',
+                    'Kiwi PyCon 是 Python 社群聚集在一起的聚會和交流，分享經驗，互相學習，並建立持久的社群關係。' +
+                    '當我們聚集時，我們就變成了 Kiwi PyCon。' +
+                    'Kiwi PyCon 在全國各地移動，讓所有社群成員有機會在其居住地附近參加會議。' +
+                    '今年 Kiwi PyCon 將採用混合模式進行！' +
+                    '將在 Ōtautahi Christchurch 的藝術中心和線上同時進行，並使用 Venueless 作為我們的線上平台。',
             },
             {
                 tag: 'Australia',
@@ -145,57 +174,6 @@ export default genI18nMessages({
                     'PyCon Australia 是 Python 社群的國際研討會，' +
                     '匯集了專業、學生和愛好者開發者、系統管理員和操作人員、學生、教育工作者、科學家、統計學家以及其他許多熱愛 Python 的人。' +
                     'PyCon Australia 透過 Python 專家和核心開發人員的演示、教程和小組會議，以及他們所依賴的函式庫與框架，促進澳洲的 Python 開發者更多討論與交流。',
-            },
-            {
-                tag: 'Japan',
-                title: 'PyCon Japan',
-                description:
-                    'PyCon Japan 2022 是日本最大的 Python 會議，提供 Python 用戶聚集和交流 Python 相關與使用 Python 軟體的資訊。' +
-                    'PyCon Japan 在 2020 年和 2021 年分別以線上和混合方的式舉辦，但在 2022 年，我們將再次在線下相聚，交流 Python 在各領域的知識和資訊並結交新朋友。' +
-                    '我們的目標是創造一個可以與社群、工作和商業機會聯繫的地方。我們仍在敲定 PyCon JP 2022 的 Logo，所以請再給我們一些時間。',
-            },
-            {
-                tag: 'Thailand',
-                title: 'PyCon Thailand',
-                description:
-                    'PyCon Thailand 是由泰國曼谷 Python 社群成員推動的社群活動，整個籌備團隊都是志工。' +
-                    'PyCon Thailand 是一項非營利性活動，任何從門票銷售或贊助中獲得的資金都將投入到這次或未來的活動中，以發展社群。' +
-                    'PyCon Thailand 與泰國的許多其他開發者活動不同，它既是非營利性的，又具有可負擔的票價。' +
-                    '該活動可以是為公司提供有價值的培訓和網路的專業會議。 PyCon Thailand 還為泰國的非專業 Python 社群提供資金支持。',
-            },
-            {
-                tag: 'Malaysia',
-                title: 'PyCon Malaysia',
-                description:
-                    'PyCon Malaysia 的籌備社群總是很小而且是封閉的。' +
-                    '大多數的委員會成員都參與了多個 PyCon Malaysia。' +
-                    '一些前輩，如 Iqbal、Swee Meng、Kei Yan 和 Kinabalu Coders 的成員一直在幫助舉辦很多項活動。' +
-                    '2022 年，主要的委員會成員由 Hilmi、James 和 Ivy 所組成。',
-            },
-            {
-                tag: 'Russia',
-                title: 'PyCon Russia',
-                description:
-                    'PyCon Russia 是一個俄羅斯得 Python 開發者大會，自 2013 年起由 IT 開發人員主辦。' +
-                    '2018 年聚集了來自莫斯科、聖彼得堡、葉卡捷琳堡、喀山、下諾夫哥羅德、薩馬拉、彼爾姆等城市的 600 人。' +
-                    'PyCon Russia 有一種特殊的形式：' +
-                    '參與者和演講者在戶外度過兩天，聆聽來自俄羅斯和世界各地的演講者的議程，在閃電演講 (Lightning Talks) 上表演，晚上在營火和吉他下的歌聲中享受樂趣。',
-            },
-            {
-                tag: 'HongKong',
-                title: 'PyCon Hong Kong',
-                description:
-                    '在東京參與 PyCon APAC 2013 期間，Sammy 會見了 PyCon APAC 的其他組織者，' +
-                    '發現 PyCon 已經在除香港以外的亞洲多個城市舉行。' +
-                    '返回香港後，Sammy 和 Open Source Hong Kong 於 2015 年組織了第一屆 PyCon Hong Kong。' +
-                    '該活動成為促進和推動 Python 開發和使用在該市擴散的催化劑之一。',
-            },
-            {
-                tag: 'Singapore',
-                title: 'PyCon Singapore',
-                description:
-                    'PyCon Singapore 是一個關於 Python 的研討會。' +
-                    '這是由新加坡 Python 使用者所組成，為動 Python 社群努力。',
             },
             {
                 tag: 'Indonesia',
@@ -207,12 +185,28 @@ export default genI18nMessages({
                     '加入我們一起推廣 Python Everywhere。',
             },
             {
-                tag: 'Korea',
-                title: 'PyCon Korea',
+                tag: 'Singapore',
+                title: 'PyCon Singapore',
                 description:
-                    'PyCon Korea 是由社群成員組織的非營利性、面向開發者的研討會，具有與會議管理的公司或組織不同的特點。' +
-                    '所有參與 PyCon Korea 的與會者，包括演講者、教學主持人、志工和組織者皆以非盈利目的，' +
-                    '所有成員都是志工。我們組織起來以擴大 Python 的使用範圍並活絡社群。',
+                    'PyCon Singapore 是一個關於 Python 的研討會。' +
+                    '這是由新加坡 Python 使用者所組成，為動 Python 社群努力。',
+            },
+            {
+                tag: 'Malaysia',
+                title: 'PyCon Malaysia',
+                description:
+                    'PyCon MY 2022 與前幾屆 PyCon MY 相比將有所不同。' +
+                    '除了在沙巴哥打京那巴魯欣賞風景外，我們還將舉辦一個小型聚會，為與會者提供不同的衝刺項目。' +
+                    '這也是一項獨家活動，因為我們將只允許 80 名與會者參加本次 PyCon。',
+            },
+            {
+                tag: 'Thailand',
+                title: 'PyCon Thailand',
+                description:
+                    'PyCon Thailand 是由泰國曼谷 Python 社群成員推動的社群活動，整個籌備團隊都是志工。' +
+                    'PyCon Thailand 是一項非營利性活動，任何從門票銷售或贊助中獲得的資金都將投入到這次或未來的活動中，以發展社群。' +
+                    'PyCon Thailand 與泰國的許多其他開發者活動不同，它既是非營利性的，又具有可負擔的票價。' +
+                    '該活動可以是為公司提供有價值的培訓和網路的專業會議。 PyCon Thailand 還為泰國的非專業 Python 社群提供資金支持。',
             },
             {
                 tag: 'India',
@@ -223,11 +217,13 @@ export default genI18nMessages({
                     '這是一場由志工驅動的會議，旨在促進 Python 的採用、使用和開發。',
             },
             {
-                tag: 'Philippines',
-                title: 'PyCon Philippines',
+                tag: 'Russia',
+                title: 'PyCon Russia',
                 description:
-                    'PyCon Philippines 是一個以 Python 為中心的志工組織的非營利性會議。' +
-                    '研討會的目標是提供一個可以探索、討論和練習 Python 和周邊技術的場所。',
+                    'PyCon Russia 是一個俄羅斯得 Python 開發者大會，自 2013 年起由 IT 開發人員主辦。' +
+                    '2018 年聚集了來自莫斯科、聖彼得堡、葉卡捷琳堡、喀山、下諾夫哥羅德、薩馬拉、彼爾姆等城市的 600 人。' +
+                    'PyCon Russia 有一種特殊的形式：' +
+                    '參與者和演講者在戶外度過兩天，聆聽來自俄羅斯和世界各地的演講者的議程，在閃電演講 (Lightning Talks) 上表演，晚上在營火和吉他下的歌聲中享受樂趣。',
             },
         ],
         website: '官方網站',
