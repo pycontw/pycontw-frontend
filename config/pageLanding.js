52a270e4624352c52ad293f40539ab80c6b0cba5
ylu08 <yungsheng.lu@yahooinc.com>
2022-12-07 19:16:05 +0800

    style: landing page

---
+++ config/pageLanding.js
@@ -0,0 +1,14 @@
+export const landingButtonConfig = {
+    JOIN_US: {
+        path: 'https://forms.gle/vLz3Xev8tUpnRqacA',
+        textKey: 'joinUs',
+    },
+    CFP: {
+        path: '/speaking/cfp',
+        textKey: 'callForProposals',
+    },
+    CHECK_EVENTS: {
+        path: '/conference/schedule',
+        textKey: 'checkEvents',
+    },
+}
