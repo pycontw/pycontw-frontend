3bacb29376d0b6dbd0347ab102788d18d464a369
ylu08 <yungsheng.lu@yahooinc.com>
2023-02-16 13:39:59 +0800

    feat: update description of submitting proposal

--- i18n/speaking/cfp.i18n.js
+++ i18n/speaking/cfp.i18n.js
@@ -22,6 +22,7 @@ export default genI18nMessages({
                 description: [
                     'You need to {signUp} for a new account on our system. With an activated account, you can fill up the speaker profile and create new proposals through the {proposalSystem} page.',
                     'We encourage you to submit the proposal as early as possible. You are welcomed to submit multiple proposals.',
+                    'Since COVID-19 become stable, we recommend attending the meeting venue in person. If you have personal reasons, you may ask to give your talk or tutorial remotely.',
                 ],
                 steps: [],
             },
@@ -151,6 +152,7 @@ export default genI18nMessages({
                 title: '提交稿件',
                 description: [
                     '您需要先在系統上 {signUp} 一個新的帳號。在啟用您的帳號之後，就可以填寫講者資訊，並從 {proposalSystem} 頁面中建立新的投搞。我們強烈建議您儘早送出您的投稿。',
+                    '因應 COVID-19 疫情逐漸穩定，今年演講與專業課程將會以實體會議形式舉辦，建議講者來到現場與會眾互動。若有不可抗力之因素，您仍可以依照自身狀況選擇是否親自前來。',
                 ],
                 steps: [],
             },
