32fcdd122aecf882e37e8276dfc4fac2ade922f6
svyeh <sivanyeh@gmail.com>
2024-01-27 13:46:27 +0800

    update sponsorship and og img

--- i18n/sponsor/index.i18n.js
+++ i18n/sponsor/index.i18n.js
@@ -4,10 +4,9 @@ export default genI18nMessages({
     'en-us': {
         title: 'Why Sponsorship',
         intro: [
-            'PyCon Taiwan has held over 10 annual conferences to date. Last year, we invited 5 keynote speakers, 50 general talks, {br}' +
-                'and 3 tutorials, totaling over 36 hours of technical presentations. {br}' +
-                "At the same time, our community's social media activity has also increased, with a growth of 10,600 followers, {br}" +
-                'a 45.8% increase. In addition, the podcast has been downloaded 8,560 times. ' +
+            'PyCon Taiwan has held over 12 annual conferences to date. Last year, we invited 2 keynote speakers, 27 general talks, 3 tutorials and 1 panel totaling over 36 hours of technical presentations. {br}' +
+                "At the same time, our community's social media activity has also increased, with a growth of 11,162 followers. {br}" +
+                'In addition, the podcast has been downloaded 12,000 times. ' +
                 'These statistics demonstrate the success of PyCon Taiwan.With over 150 volunteers, ' +
                 'we have brought countless technical skills and inspiration to the Python community.',
         ],
@@ -19,19 +18,19 @@ export default genI18nMessages({
             },
             {
                 achievement: 'Participants',
-                achievementNumber: '800',
+                achievementNumber: '550',
                 achievementUnit: 'People',
             },
             {
                 achievement: 'Social Media',
-                achievementNumber: '10K',
+                achievementNumber: '11K',
                 achievementUnit: 'Followers',
             },
         ],
         serviceIntro: [
             'PyCon Taiwan is driven by our members and you, our awesome sponsor partners!{br}' +
                 'Your generous support is the motivation to help us move forward.{br}' +
-                'PyConTW 2023 calls for your sponsorship to enlarge our community.',
+                'PyConTW 2024 calls for your sponsorship to enlarge our community.',
             'Meanwhile, we also provide the benefit for our sponsors below:',
         ],
         services: [
@@ -54,15 +53,15 @@ export default genI18nMessages({
             title: 'Sponsorship',
             description:
                 'Your generous support is the motivation to help us move forward. ' +
-                'PyConTW 2023 calls for your sponsorship to enlarge our community.',
+                'PyConTW 2024 calls for your sponsorship to enlarge our community.',
         },
     },
     'zh-hant': {
         title: '贊助召集令',
         intro: [
-            'PyCon Taiwan 迄今已舉辦超過 10 場年度會議。去年我們邀請 5 場主題演講、50 場一般演講以及 3 場專業課程，{br}' +
-                '總計超過 36 小時的技術演講。同時，我們的社群媒體活躍度也隨之提高，追蹤人數增加了 10,600 位，{br}' +
-                '增長了 45.8%。此外，Podcast 下載量高達 8,560 次。這些數據充分展現了 PyCon Taiwan 的成功。 {br}' +
+            'PyCon Taiwan 迄今已舉辦超過 12 場年度會議。去年我們邀請 2 場主題演講、27 場一般演講、1 場專業課程以及 1 場專業論壇，{br}' +
+                '總計超過 36 小時的技術演講。同時，我們的社群媒體活躍度也隨之提高，追蹤人數增加到了 11162 位。{br}' +
+                '此外，Podcast 下載量高達 12000次。這些數據充分展現了 PyCon Taiwan 的成功。 {br}' +
                 '我們擁有超過 150 位志工，為 Python 社群帶來了無數的技術與靈感。',
         ],
         milestone: [
@@ -73,19 +72,19 @@ export default genI18nMessages({
             },
             {
                 achievement: '參與會眾',
-                achievementNumber: '800',
+                achievementNumber: '550',
                 achievementUnit: '人以上',
             },
             {
                 achievement: '社群媒體',
-                achievementNumber: '10K',
+                achievementNumber: '11K',
                 achievementUnit: '追蹤人數',
             },
         ],
         serviceIntro: [
             '您的支持是推進 PyCon Taiwan 的強大動力。{br}' +
                 'PyCon Taiwan 在此召集您的加入，透過贊助以具體行動支持 Python 社群的成長！{br}' +
-                '透過參與 PyConTW 2023，我們也提供贊助廠商以下的服務：',
+                '透過參與 PyConTW 2024，我們也提供贊助廠商以下的服務：',
         ],
         services: [
             '接觸專業人士',
@@ -94,7 +93,7 @@ export default genI18nMessages({
             '建立人才網絡',
         ],
         summary:
-            '若想了解 PyConTW 2023 完整的贊助方案，歡迎點擊下方按鈕填寫表單留下您的聯絡資料' +
+            '若想了解 PyConTW 2024 完整的贊助方案，歡迎點擊下方按鈕填寫表單留下您的聯絡資料' +
             '索取贊助書或是直接透過贊助組信箱 {contact} 聯絡我們，讓我們了解貴單位的需求！',
         terms: {
             contact: 'sponsorship@python.tw',
