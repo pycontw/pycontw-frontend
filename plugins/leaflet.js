20baac0105da505f42a714aed8043a8d87c17b3d
rockleona <rockleonjp@gmail.com>
2024-02-05 16:56:27 +0800

    fix: leafleat module import error in ssr mode

--- plugins/leaflet.js
+++ plugins/leaflet.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import {
+    LMap,
+    LControl,
+    LTileLayer,
+    LMarker,
+    LControlZoom,
+    LControlLayers,
+    LTooltip,
+} from 'vue2-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 Vue.component('l-map', LMap)
 Vue.component('l-tile-layer', LTileLayer)
 Vue.component('l-marker', LMarker)
+Vue.component('l-control', LControl)
+Vue.component('l-control-zoom', LControlZoom)
+Vue.component('l-conrtol-layers', LControlLayers)
+Vue.component('l-tooltip', LTooltip)
