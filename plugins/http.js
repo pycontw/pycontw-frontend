54b01e5bf3490d4c28d6d76f0d47d6ef8eb61969
SeanHsieh <t4612310@gmail.com>
2024-06-08 22:32:05 +0800

    fix(server-fetch): add host to header to resolve be allow host 400

--- plugins/http.js
+++ plugins/http.js
@@ -1,6 +1,9 @@
 export default function ({ $http }) {
     const token = process.env.AUTH_TOKEN || ''
     $http.setToken(token, 'Token')
+    if (process.env.DEPLOY_ENV) {
+        $http.setHeader('Host', process.env.DEPLOY_ENV)
+    }
     $http.onError((error) => {
         return error.response.data
     })
