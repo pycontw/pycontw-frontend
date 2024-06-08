b2d068e523865ac60f5519ed156fd86688b02c95
SeanHsieh <t4612310@gmail.com>
2024-06-08 22:36:39 +0800

    fix: rename env and add some comments

--- plugins/http.js
+++ plugins/http.js
@@ -1,8 +1,9 @@
 export default function ({ $http }) {
     const token = process.env.AUTH_TOKEN || ''
     $http.setToken(token, 'Token')
-    if (process.env.DEPLOY_ENV) {
-        $http.setHeader('Host', process.env.DEPLOY_ENV)
+    // Set the host name for the API header for the server side to avoid api 400 error.
+    if (process.server && process.env.HOST_NAME) {
+        $http.setHeader('Host', process.env.HOST_NAME)
     }
     $http.onError((error) => {
         return error.response.data
