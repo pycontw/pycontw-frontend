d5993b01f1adc12c7f2a48f2d97d134cce124b78
svyeh <sivanyeh@gmail.com>
2024-06-10 21:58:39 +0800

    fit: outdated info
    Remove 2023 information from index, keynote and sponsor.

--- components/intro/Intro.i18n.js
+++ components/intro/Intro.i18n.js
@@ -5,7 +5,7 @@ export default genI18nMessages({
         milestone: [
             {
                 achievement: 'Hold',
-                achievementNumber: '11',
+                achievementNumber: '12',
                 achievementUnit: 'years',
             },
             {
@@ -15,7 +15,7 @@ export default genI18nMessages({
             },
             {
                 achievement: 'Audience',
-                achievementNumber: '800',
+                achievementNumber: '550',
                 achievementUnit: 'People',
             },
         ],
@@ -26,13 +26,7 @@ export default genI18nMessages({
             ' of the Python programming language. It is held by enthusiasts and focuses on' +
             ' Python technology and its versatile applications. We welcome people who are' +
             ' interested in Python to join PyCon Taiwan to share knowledge, exchange ideas,' +
-            ' make connections and to help us grow our network.' +
-            ' In 2023, We will hold a physical conference again!' +
-            ' With "Rebirth" as the main axis, ' +
-            'we encourage Pythonistas to get out of the virtual world' +
-            ' and lead everyone back to the venue.' +
-            ' We look forward to new and old audiences bringing more' +
-            ' innovative ideas to the Python community, and exciting different sparks.',
+            ' make connections and to help us grow our network.',
         whatIsPyCon: 'What is PyCon',
         whatIsPyConContent:
             'PyCon, also known as the Python Annual Conference, is the annual event of the global Python community.' +
@@ -43,7 +37,7 @@ export default genI18nMessages({
         milestone: [
             {
                 achievement: '持續舉辦',
-                achievementNumber: '11',
+                achievementNumber: '12',
                 achievementUnit: '年',
             },
             {
@@ -53,7 +47,7 @@ export default genI18nMessages({
             },
             {
                 achievement: '參與會眾',
-                achievementNumber: '800',
+                achievementNumber: '550',
                 achievementUnit: '人以上',
             },
         ],
@@ -62,11 +56,7 @@ export default genI18nMessages({
         whatIsPyConTWContent:
             'PyCon Taiwan 為一年一度由愛好者舉辦、討論並提倡使用 Python 程式語言的會議，' +
             '聚焦在 Python 技術與其多樣的可能應用的交流。我們歡迎所有對 Python ' +
-            '有興趣的朋友一同加入 PyCon Taiwan 來分享所學、交換想法、並且認識更多同好。' +
-            '這兩年在疫情的衝擊下，開發者們面對面交流的機會減少許多。' +
-            '2023 年我們將再次在中研院舉辦實體年會，' +
-            '以「重生」為主軸，鼓勵 Pythonista 們走出虛擬世界，帶領大家回到現場。' +
-            '期待新舊會眾們為 Python 社群帶來更多創新想法，激盪出不一樣的火花。',
+            '有興趣的朋友一同加入 PyCon Taiwan 來分享所學、交換想法、並且認識更多同好。',
         whatIsPyCon: '何謂 PyCon',
         whatIsPyConContent:
             'PyCon，亦即 Python 年會，是全球 Python 社群的年度盛會。' +
