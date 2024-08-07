7231247f814aeebf8952acf32b509810cb3b76c0
Matt Wang <mattwang44@gmail.com>
2024-08-08 07:35:43 +0800

    Revert "Merge pull request #562 from pycontw/feat/close-tickets"
    This reverts commit d2c9661459da3320c02d9ffb881bdea16d379998, reversing
    changes made to c1e6c8d63e3784d05730f03c47a715776924f26c.

--- configs/pageTickets.js
+++ configs/pageTickets.js
@@ -36,7 +36,7 @@ export const ticketInfoConfig = [
             'lunchBoxAndDesserts',
             'conferencePass',
         ],
-        statusI18nKey: ticketSellingStatus.ENDED,
+        statusI18nKey: ticketSellingStatus.SELLING,
         link: pageLinks.kktixIndividual2024,
     },
     {
@@ -62,7 +62,7 @@ export const ticketInfoConfig = [
             'conferencePass',
             'earlyRegularStageTShirt',
         ],
-        statusI18nKey: ticketSellingStatus.ENDED,
+        statusI18nKey: ticketSellingStatus.SELLING,
         link: pageLinks.kktixIndividual2024,
     },
     {
@@ -75,7 +75,7 @@ export const ticketInfoConfig = [
             'conferencePass',
             'tShirt',
         ],
-        statusI18nKey: ticketSellingStatus.ENDED,
+        statusI18nKey: ticketSellingStatus.SELLING,
         link: pageLinks.kktixIndividual2024,
     },
     {
