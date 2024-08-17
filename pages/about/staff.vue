09d51dc5b466c0817e0f512a5689e81a6d8869df
svyeh <sivanyeh@gmail.com>
2024-08-17 13:31:15 +0800

    feat(staff): add name and img
    Example for staff adding

--- pages/about/staff.vue
+++ pages/about/staff.vue
@@ -56,7 +56,7 @@ export default {
                 everywhere: [],
                 program: [],
                 pycast: [],
-                development_web: [],
+                development_web: [{ name: 'Benson', photo: 'web/Benson.jpg' }],
                 development_data: [],
                 development_infra: [],
                 venue: [],
