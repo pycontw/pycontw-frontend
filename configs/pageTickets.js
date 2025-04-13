d5072c4cc4eb3397d0b0c99e083da8e223107810
Weilong <fan01856472@gmail.com>
2025-04-13 11:51:30 +0800

    enable the tickets page

--- configs/pageTickets.js
+++ configs/pageTickets.js
@@ -8,116 +8,102 @@ export const ticketSellingStatus = {
 
 export const pageLinks = {
     codeOfConduct: '/about/code-of-conduct',
-    disabilityCertification: 'https://www.mohw.gov.tw/cp-88-235-1-45.html',
+    disabilityCertification: 'https://www.mohw.gov.tw/cp-88-235-1-45.html', // TODO: TBC
     everybodyContributesPrinciple:
-        'http://jessenoller.com/blog/2011/05/25/pycon-everybody-pays',
-    groupBuyCorporateTicketsForm: 'https://forms.gle/HRBH3KfDpFm2Y4nK6',
+        'http://jessenoller.com/blog/2011/05/25/pycon-everybody-pays', // TODO: demand for updating the broken link
+    corporateGroupTicketsForm: 'https://forms.gle/HRBH3KfDpFm2Y4nK6', // TODO: TBC
     identityVerificationOfPeopleWithDisability:
-        'https://kktix.com/account/identities',
-    kktixCorporate2024: 'https://pycontw.kktix.cc/events/2024-corporate',
-    kktixIndividual2024: 'https://pycontw.kktix.cc/events/2024-individual',
+        'https://kktix.com/account/identities', // TODO: TBC
+    kktixCorporate2025: 'https://pycontw.kktix.cc/events/2025-corporate', // TODO: TBC
+    kktixPremium2025: 'https://pycontw.kktix.cc/events/2025-premium', // TODO: TBC
+    kktixIndividual2025: 'https://pycontw.kktix.cc/events/2025-individual', // TODO: TBC
+    kktixAccessible2025: 'https://pycontw.kktix.cc/events/2025-accessibility', // TODO: TBC
     kktixOnBehalf: 'https://support.kktix.com/knowledgebase/articles/356418',
-    kktixReserved2024: 'https://pycontw.kktix.cc/events/2024-reserved',
-    nonCorporateTicketsInvoicing: 'https://forms.gle/neJhs8xKQ1gykQQB8',
+    nonCorporateTicketsInvoicing: 'https://forms.gle/neJhs8xKQ1gykQQB8', // TODO: outdated form
     privacyPolicy: '/about/privacy-policy',
     organizerEmail: 'mailto:organizers@python.tw',
     officialWebsite: '/',
+    financialAid: '/registration/financial-aid',
 }
-
+// TODO: adjust
 export const ticketInfoConfig = [
     {
-        titleI18nKey: 'tickets.premium',
-        price: 'NT$ 5,500',
-        image: require('~/static/img/registration/tickets/regular.svg'),
+        titleI18nKey: 'tickets.corporate',
+        price: 'NT$ 5800',
+        image: require('~/static/img/registration/tickets/corporate.svg'),
         featuresI18nKey: [
-            'support',
-            'thankYouList',
-            'tShirt',
+            'includeSwag',
             'lunchBoxAndDesserts',
             'conferencePass',
+            'reimbursementReceipt',
         ],
         statusI18nKey: ticketSellingStatus.SELLING,
-        link: pageLinks.kktixIndividual2024,
+        link: pageLinks.kktixCorporate2025,
+    },
+    {
+        titleI18nKey: 'tickets.corporateGroup',
+        price: 'NT$ 5500',
+        image: require('~/static/img/registration/tickets/corporate.svg'),
+        featuresI18nKey: [
+            'includeSwag',
+            'lunchBoxAndDesserts',
+            'conferencePass',
+            'reimbursementReceipt',
+            'dedicatedService',
+            'corporateGroupTicketLimit',
+        ],
+        statusI18nKey: ticketSellingStatus.APPLY,
+        link: pageLinks.corporateGroupTicketsForm,
+    },
+    {
+        titleI18nKey: 'tickets.premium',
+        price: 'NT$ 7777',
+        image: require('~/static/img/registration/tickets/sponsor.svg'),
+        featuresI18nKey: [
+            'includeSwag',
+            'lunchBoxAndDesserts',
+            'conferencePass',
+            'invitationOfSpeakerNight',
+            'individualRecognition',
+        ],
+        statusI18nKey: ticketSellingStatus.SELLING,
+        link: pageLinks.kktixPremium2025,
     },
     {
         titleI18nKey: 'tickets.individual',
-        prices: [
-            {
-                titleI18nKey: 'sellingStage.earlyStage',
-                price: 'NT$ 2,790',
-            },
-            {
-                titleI18nKey: 'sellingStage.regularStage',
-                price: 'NT$ 3,790',
-            },
-            {
-                titleI18nKey: 'sellingStage.finalStage',
-                price: 'NT$ 4,290',
-            },
-        ],
+        price: 'NT$ 3790',
         image: require('~/static/img/registration/tickets/regular.svg'),
         featuresI18nKey: [
-            'earlyStageDiscount',
+            'includeSwag',
             'lunchBoxAndDesserts',
             'conferencePass',
-            'earlyRegularStageTShirt',
         ],
         statusI18nKey: ticketSellingStatus.SELLING,
-        link: pageLinks.kktixIndividual2024,
+        link: pageLinks.kktixIndividual2025,
     },
     {
-        titleI18nKey: 'tickets.concession',
-        price: 'NT$ 1,895',
+        titleI18nKey: 'tickets.accessibility',
+        price: 'NT$ 2690',
         image: require('~/static/img/registration/tickets/regular.svg'),
         featuresI18nKey: [
-            'disabilitiesOnly',
+            'includeSwag',
             'lunchBoxAndDesserts',
             'conferencePass',
-            'tShirt',
+            'accessibilityTicketLimit',
         ],
         statusI18nKey: ticketSellingStatus.SELLING,
-        link: pageLinks.kktixIndividual2024,
+        link: pageLinks.kktixAccessible2025,
     },
     {
-        titleI18nKey: 'tickets.corporate',
-        prices: [
-            {
-                titleI18nKey: 'sellingStage.regularStage',
-                price: 'NT$ 5,800',
-            },
-            {
-                titleI18nKey: 'sellingStage.finalStage',
-                price: 'NT$ 6,500',
-            },
-        ],
-        image: require('~/static/img/registration/tickets/corporate.svg'),
+        titleI18nKey: 'tickets.financialAid',
+        price: '',
+        image: require('~/static/img/registration/tickets/regular.svg'),
         featuresI18nKey: [
-            'reimbursementReceipt',
-            'regularStageTShirt',
+            'excludeSwag',
             'lunchBoxAndDesserts',
             'conferencePass',
         ],
         statusI18nKey: ticketSellingStatus.SELLING,
-        link: pageLinks.kktixCorporate2024,
-    },
-    {
-        titleI18nKey: 'tickets.groupBuyCorporate',
-        prices: [
-            {
-                titleI18nKey: 'sellingStage.regularStage',
-                price: 'NT$ 5,220',
-            },
-        ],
-        image: require('~/static/img/registration/tickets/corporate.svg'),
-        featuresI18nKey: [
-            'reimbursementReceipt',
-            'groupBuyDiscount',
-            'earlyStageTShirt',
-            'lunchBoxAndDesserts',
-            'conferencePass',
-            'groupBuyApply',
-        ],
-        statusI18nKey: ticketSellingStatus.APPLY,
-        link: pageLinks.groupBuyCorporateTicketsForm,
+        link: pageLinks.financialAid,
     },
 ]
