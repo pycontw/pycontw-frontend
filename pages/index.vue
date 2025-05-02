bbdca4e278c656f340fee57e956293933c1ad956
Weilong <fan01856472@gmail.com>
2025-05-02 22:31:34 +0800

    adjust date desc and update btn link on the index page

--- pages/index.vue
+++ pages/index.vue
@@ -156,7 +156,7 @@ export default {
     data() {
         return {
             isOpened: false,
-            primaryButton: landingButtonConfig.CFP,
+            primaryButton: landingButtonConfig.BUY_TICKETS,
             secondaryButton: landingButtonConfig.SECONDARY_JOIN_US,
             selectedSponsor: {},
         }
