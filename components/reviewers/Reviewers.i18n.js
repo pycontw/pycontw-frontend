5b372409dd463712c24bd2331c9be2b48ed6e1f1
Rudolf Chen <rudd6617@gmail.com>
2025-06-21 16:09:03 +0800

    chore: remove redundant

--- components/reviewers/Reviewers.i18n.js
+++ components/reviewers/Reviewers.i18n.js
@@ -3,15 +3,8 @@ import { genI18nMessages } from '~/utils/i18n.utils'
 export default genI18nMessages({
     'en-us': {
         reviewers: 'Reviewers',
-        whatIsPyConContent:
-            'PyCon, also known as the Python Annual Conference, is the annual event of the global Python community.' +
-            ' The original PyCon was formed in North America in 2003, and now there' +
-            ' are many other conferences being run in the PyCon spirit around the world.',
     },
     'zh-hant': {
         reviewers: '審稿人員',
-        whatIsPyConContent:
-            'PyCon，亦即 Python 年會，是全球 Python 社群的年度盛會。' +
-            '最初於 2003 成立於北美，現在世界各地皆有以 PyCon 精神而成立的會議。',
     },
 })
