e46dc6e45ecc8d4409372b35230576bc492bab7d
lisachouHC <105439296+lisachouHC@users.noreply.github.com>
2025-07-19 00:22:20 +0800

    修改br 格式

--- pages/events/open-spaces.vue
+++ pages/events/open-spaces.vue
@@ -26,8 +26,8 @@
                             require(`~/static/img/events/overview/open-space-location.png`)
                         "
                     />
-                    <br>
-                    <br>
+                    <br />
+                    <br />
                     <img
                         v-if="openSpace.tag === 'location'"
                         :src="
