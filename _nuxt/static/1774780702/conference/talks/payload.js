e5ac35869c1028fd50fd7ba248b3b0333cb9ac28
serkodev <serko.dev@gmail.com>
2026-03-29 10:38:48 +0000

    Deploying to gh-pages from  @ 8688d0f2755a491145ddbbc4a6a5f700a25c61da 🚀

---
+++ _nuxt/static/1774780702/conference/talks/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/conference/talks", {data:[{eventType:"talks",speechesData:[]}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1774780702/conference/tutorials/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/conference/tutorials", {data:[{eventType:"tutorials",speechesData:[]}],fetch:{},mutations:[]});
