ce043ccca80ce1fe3e0f2af972ddb33705b23e00
SerKo <serko.dev@gmail.com>
2026-07-20 20:50:25 +0800

    fix: locale bar item popover on mobile hover

--- app/components/LocaleBarItem.vue
+++ app/components/LocaleBarItem.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 const { locale, locales } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
+const isSupportHover = useMediaQuery('(hover: hover)')
 const open = ref(false)
 </script>
 
 <template>
-  <UPopover v-model:open="open" mode="hover" arrow :content="{ hideWhenDetached: true }">
+  <UPopover v-model:open="open" :mode="isSupportHover ? 'hover' : 'click'" arrow :content="{ hideWhenDetached: true }">
     <UButton color="neutral" variant="ghost" icon="i-lucide:languages" />
 
     <template #content>
