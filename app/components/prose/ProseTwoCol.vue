3e9bba431ee93d5d98f12696093a80e292f8b25c
SerKo <serko.dev@gmail.com>
2026-07-27 18:42:14 +0800

    style: update colors

--- app/components/prose/ProseTwoCol.vue
+++ app/components/prose/ProseTwoCol.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="lg:grid lg:grid-cols-[1fr_5fr] lg:my-0 my-8 gap-4">
-    <div class="text-neutral-200 text-2xl font-semibold">
+    <div class="text-highlighted text-2xl font-semibold">
       <slot name="title" mdc-unwrap="p">
         <MDC v-if="title" :value="title" />
       </slot>
